d9568194e7eb28c99fdd4de101ca65802aad73fc
Felix Oesterle <felix@alpinepics.com>
2020-07-24 21:31:57 +0200

    Modify pep8speak and minor change for test

--- avaframe/tmp1Ex/tmp1Ex.py
+++ avaframe/tmp1Ex/tmp1Ex.py
@@ -5,6 +5,7 @@ This is the template for new modules, with the bare minimal required files
 # Local import
 import avaframe.tmp1Ex.tmp1ExCfg as conf
 
+
 def tmp1ExMain():
     """ Main function for module tmp1Example"""
 
