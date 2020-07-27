aa66c80095c6a539db5e6a9bc13f6a800e4b7512
Felix Oesterle <felix@alpinepics.com>
2020-07-27 13:05:05 +0200

    Add missing variables

--- avaframe/runCom2AB.py
+++ avaframe/runCom2AB.py
@@ -7,11 +7,10 @@ from avaframe.com2AB import com2AB
 import avaframe.com2AB.com2ABCfg as conf
 
 print("[com2AB] Running com2ABMain model on test case DEM : ",
-        conf.DGMSource, 'with profile:', conf.ProfileLayer)
+      conf.DGMSource, 'with profile:', conf.ProfileLayer)
 
 [header, rasterdata] = com2AB.ReadRaster(conf.DGMSource)
 avapath = com2AB.ReadAvaPath(conf.ProfileLayer, header)
 splitPoint = com2AB.ReadSplitPoint(conf.SplitPointSource, header)
 
 # com2AB.com2ABMain(header, rasterdata, avapath, splitPoint, conf.saveOutPath, conf.smallAva)
-
