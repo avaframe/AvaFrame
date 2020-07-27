f4b96a867c2ae01d7f40e27a3665123bd4b491ff
Felix Oesterle <felix@alpinepics.com>
2020-07-27 22:28:11 +0200

    Add logging example; closes #8

--- avaframe/runTmp1Ex.py
+++ avaframe/runTmp1Ex.py
@@ -1,11 +1,17 @@
 """Run script for module template"""
 
+import logging
+import sys
+
 # Local imports
 import avaframe as af
-
 from avaframe.tmp1Ex import tmp1Ex
+
+# create logger, set to logging.DEBUG to see all messages
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format='%(module)s:%(levelname)s - %(message)s')
 
 # Different ways to call functions
 tmp1Ex.tmp1ExMain()
 
-af.tmp1Ex.tmp1Ex.tmp1ExMain()
+# af.tmp1Ex.tmp1Ex.tmp1ExMain()
