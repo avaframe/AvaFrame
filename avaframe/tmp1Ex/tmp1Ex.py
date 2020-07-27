dbf4bc2ca735e54efdc4adee78c30b553359eeaa
Felix Oesterle <felix@alpinepics.com>
2020-07-27 22:28:11 +0200

    Add logging example; closes #8

--- avaframe/tmp1Ex/tmp1Ex.py
+++ avaframe/tmp1Ex/tmp1Ex.py
@@ -2,12 +2,19 @@
 This is the template for new modules, with the bare minimal required files
 """
 
+import logging
+
 # Local import
 import avaframe.tmp1Ex.tmp1ExCfg as conf
+
+# create local logger
+# change log level in calling module to DEBUG to see log messages
+log = logging.getLogger(__name__)
 
 
 def tmp1ExMain():
     """ Main function for module tmp1Example"""
 
     print('In tmp1Example')
-    # print('Input directory:', conf.inputDir)
+    log.debug('Here')
+    log.debug('Input directory %s', conf.inputDir)
