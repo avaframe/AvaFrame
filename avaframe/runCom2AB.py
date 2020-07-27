670bbb040d9dfd9e1c23aad5ba03b6f5f1263451
Felix Oesterle <felix@alpinepics.com>
2020-07-27 12:56:07 +0200

    Cleanup code; break out parameter setting function com2AB
            - move main run script to runCom2AB.py

---
+++ avaframe/runCom2AB.py
@@ -0,0 +1,17 @@
+"""Run script for module com2AB
+"""
+
+# Local imports
+import avaframe as af
+from avaframe.com2AB import com2AB
+import avaframe.com2AB.com2ABCfg as conf
+
+print("[com2AB] Running com2ABMain model on test case DEM : ",
+        conf.DGMSource, 'with profile:', conf.ProfileLayer)
+
+[header, rasterdata] = com2AB.ReadRaster(conf.DGMSource)
+avapath = com2AB.ReadAvaPath(conf.ProfileLayer, header)
+splitPoint = com2AB.ReadSplitPoint(conf.SplitPointSource, header)
+
+# com2AB.com2ABMain(header, rasterdata, avapath, splitPoint, conf.saveOutPath, conf.smallAva)
+
