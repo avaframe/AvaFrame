e549f23ad857bfb4b1764528b2fa12fd9359ac5a
Matthias Tonnel <matthias@avaframe.org>
2020-07-28 15:16:50 +0200

    spliting function and updating doc

--- avaframe/com2AB/com2ABCfg.py
+++ avaframe/com2AB/com2ABCfg.py
@@ -21,9 +21,21 @@ saveOutPath = ''
 # Optional settings
 # ##########################
 
+
 # if small avalanche set is wanted
 smallAva = False
+# if custom avalanche set is wanted
+customParam = None
+# customParam = {}
+# customParam['k1'] = your value
+# customParam['k2'] = your value
+# customParam['k3'] = your value
+# customParam['k4'] = your value
+# customParam['SD'] = your value
 
+
+# resampling step [m]
+distance = 10
 
 # Some example flag
 fullLog = True
@@ -32,6 +44,6 @@ fullLog = True
 # Do not change anything below this line
 ###########################
 try:
-    from local_com2ABCfg import *
+    from .local_com2ABCfg import *
 except ImportError:
     pass
