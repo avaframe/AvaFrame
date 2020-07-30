a44263dc1317b790e6882359a8dc7d9b162854c7
Matthias Tonnel <matthias@avaframe.org>
2020-07-30 09:40:31 +0200

    Fixing AlphaBeta

--- avaframe/com2AB/com2ABCfg.py
+++ avaframe/com2AB/com2ABCfg.py
@@ -4,29 +4,51 @@ will be overridden by local_tmp1ExampleConf.py
 So copy this file to local_tmp1ExmpleConf.py, adjust your variables there
 This file serves as template file and general settings file
 """
-
+import os
+dirname = os.path.dirname(__file__)
 # ##########################
 # Required settings
 # ##########################
 
-ProfileLayer = 'YOUR PATH TO AVALANCHE PATH'
+# 'YOUR PATH TO AVALANCHE PATH'
+ProfileLayer = os.path.join(dirname, '../data/avaSlide/LINES/slideProfiles')
 
-DGMSource = 'YOUR PATH TO DEM'
+DGMSource = os.path.join(dirname, '../data/avaSlide/slideTopo.asc')  # 'YOUR PATH TO DEM'
 
-SplitPointSource = 'YOUR PATH TO SPLITPOINT'
+SplitPointSource = os.path.join(
+    dirname, '../data/avaSlide/POINTS/slidePoints')  # 'YOUR PATH TO SPLITPOINT'
 
-saveOutPath = ''
+saveOutPath = os.path.join(dirname, './Outputs/')
 
 # ##########################
 # Optional settings
 # ##########################
 
+fullLog = True
 # if small avalanche set is wanted
 smallAva = False
+# if custom avalanche set is wanted
+customParam = None
+# customParam = {}
+# customParam['k1'] = your value
+# customParam['k2'] = your value
+# customParam['k3'] = your value
+# customParam['k4'] = your value
+# customParam['SD'] = your value
 
+
+# resampling step [m]
+distance = 10
 
 # Some example flag
 fullLog = True
+
+# plot save results
+flags = {}
+flags['PlotRes'] = True
+flags['SavePlot'] = True
+flags['WriteRes'] = True
+
 
 ###########################
 # Do not change anything below this line
---
+++ avaframe/tests/test_com2AB.py
@@ -0,0 +1,82 @@
+"""Tests for module com2AB"""
+import numpy as np
+import math
+import pytest
+import avaframe.com2AB.com2AB as com2AB
+import avaframe.com2AB.com2ABCfg as conf
+import unittest
+
+# def test_setEqParameters(capfd):
+#     '''Simple test for module setEqParameters'''
+#     # small avalanche
+#     eqParameters = {}
+#     eqParameters['ParameterSet'] = 'Small avalanches'
+#     eqParameters['k1'] = 0.933
+#     eqParameters['k2'] = 0.0
+#     eqParameters['k3'] = 0.0088
+#     eqParameters['k4'] = -5.02
+#     eqParameters['SD'] = 2.36
+#
+#     eqParams = com2AB.setEqParameters(smallAva=True, customParam=None)
+#     assert(eqParams == pytest.approx(eqParameters))
+
+# def test_prepareLine(capfd):
+#     '''Simple test for function prepareLine'''
+#     header
+#     header.xllcorner = 10
+#     header.yllcorner = -15
+#     header.cellsize = 5
+#     header.ncols = 200
+#     header.nrows = 100
+#     x = np.linspace(header.xllcorner)
+#     rasterdata
+#     avapath = np.array([[], []])
+#     AvaProfile, SplitPoint, indSplit = prepareLine(
+#         header, rasterdata, avapath, splitPoint, distance=10)
+
+def test_calcAB(capfd):
+    '''Simple test for function calcAB'''
+
+    # Make a reference quadratic profile
+    B = -np.tan(np.deg2rad(45))
+    A = -B/4000
+    C = 1000
+    N = 1000
+    s = np.linspace(0.0, -B/(2*A), num=N)
+    z = np.empty(np.shape(s))
+    for i in range(N):
+        if (s[i] < (-B/(2*A))):
+            z[i] = 1*(A * s[i]*s[i]+B * s[i]+C)
+        else:
+            z[i] = 1*(-B*B / (4*A) + C)
+
+    theta_beta = 10
+    x_beta = (-np.tan(np.deg2rad(theta_beta)) - B)/(2*A)
+    y_beta = A*x_beta*x_beta + B*x_beta + C
+    beta = np.rad2deg(np.arctan2((C-y_beta), x_beta))
+    # use standard coeef
+    k1 = 1.05
+    k2 = -3130.0
+    k3 = 0.0
+    k4 = -2.38
+    SD = 1.25
+    alpha_ref = k1 * beta + k2 * 2*A + k3 * B*B/(2*A) + k4
+    SDs = [SD, -1*SD, -2*SD]
+    alphaSD_ref = k1 * beta + k2 * 2*A + k3 * B*B/(2*A) + k4 + SDs
+
+    # Using com2AB.calcAB to get the solution
+    eqIn = {}
+    eqIn['s'] = s  # curvilinear coordinate (of the x, y path)
+    eqIn['x'] = []  # x coordinate of the path
+    eqIn['y'] = []  # y coordinate of the path
+    eqIn['z'] = z  # z coordinate of the path (projection of x,y on the raster)
+    eqIn['indSplit'] = 2  # index of split point
+    eqParams = com2AB.setEqParameters(smallAva=False)
+    eqOut = com2AB.calcAB(eqIn, eqParams)
+    alpha = eqOut['alpha']
+    alphaSD = eqOut['alphaSD']
+
+    # compare results with a relative tolerance of tol
+    tol = 0.001  # here 0.1% relative diff
+    assert (alpha == pytest.approx(alpha_ref, rel=tol)) and (alphaSD[0] == pytest.approx(alphaSD_ref[0], rel=tol)) and (
+        alphaSD[1] == pytest.approx(alphaSD_ref[1], rel=tol)) and (alphaSD[2] == pytest.approx(alphaSD_ref[2], rel=tol))
