6f73d78721ceffdb27e69d406f7523012f982582
Felix Oesterle <felix@avaframe.org>
2020-08-12 23:54:36 +0200

    Complete overhaul of cfg reading and logging
             - new utils for configuration handling
             - new utils for logging
             - simplifiy runCom2AB
             - clean variable naming

---
+++ avaframe/in3Utils/logUtils.py
@@ -0,0 +1,48 @@
+""" Defining a writable object to write the config to the log file"""
+import logging
+import logging.config
+import os
+from datetime import datetime
+
+def dumpCfg(cfg, cfgName='Unspecified'):
+    '''Dumps a configParser objecct to log file'''
+
+    log = logging.getLogger('root')
+
+    log.info('Dumping cfg for: %s', cfgName)
+    for section in cfg.sections():
+        log.info('\t%s',section)
+        for key in dict(cfg[section]):
+            log.info('\t\t%s : %s',key,cfg[section][key])
+
+def initiateLogger(targetDir,logName='runLog'):
+    '''Initiates logger object based on setup in logging.conf
+
+    Input:
+    str: targetDir: folder to save log file to
+    str: logName: filename of log file; optional; defaults to runLog.log
+
+    Returns a logging object
+
+    '''
+
+    logFileName = os.path.join(targetDir, logName+'.log')
+
+    # get path of module and generate logging.conf file path
+    logConfPath = os.path.dirname(__file__)
+    logConfFile = os.path.join(logConfPath,'logging.conf')
+
+    logging.config.fileConfig(fname=logConfFile,
+                              defaults={'logfilename': logFileName},
+                              disable_existing_loggers=False)
+    log = logging.getLogger('root')
+
+    # datetime object containing current date and time
+    now = datetime.now()
+    dtString = now.strftime("%d.%m.%Y %H:%M:%S")
+    log.info('Started logging at: %s', dtString)
+    log.info('Also logging to: %s',logFileName)
+
+    return log
+
+
--- avaframe/runCom2AB.py
+++ avaframe/runCom2AB.py
@@ -1,81 +1,67 @@
 """Run script for module com2AB
 """
 
-import logging
-import logging.config
 import sys
 import os
+import logging
 
 # Local imports
 from avaframe.com2AB import com2AB
 from avaframe.out3SimpPlot import outAB
 from avaframe.in3Utils import cfgUtils
-from avaframe.in3Utils.configLogger import ConfigLogger
+from avaframe.in3Utils import logUtils
 
 #-----------Required settings-----------------
 
 # TODO: move this to a main cfg file! (alongside flags for intermediate data/results/plots)
 # and have function cfgUtils.readGlobalCfg that allows for a local_MainCfg
 
+# Avalanche directory; see doc.avaframe.org for setup
+# TODO: full path needed?
 avalancheDir = 'data/avaSlide'
+
+
+# log file name; leave empty to use default runLog.log
+logName = 'runCom2AB'
 
 #---------------------------------------------
 
-#############################################
-# Load all input Parameters from config file #
-#############################################
+# Start logging
+log = logUtils.initiateLogger(avalancheDir, logName)
+log.info('MAIN SCRIPT')
+log.info('Current avalanche: %s', avalancheDir)
 
+# Load all input Parameters from config file
 # get the configuration of an already imported module
 cfg = cfgUtils.getModuleConfig(com2AB)
 
 cfgSetup = cfg['ABSETUP']
 cfgFlags = cfg['FLAGS']
-print(cfg.sections())
 
-#########################################
-# Load all input Parameters for logging #
-#########################################
-logFileName = os.path.join(cfgPath['saveOutPath'], "runCom2AB.log")
-logging.config.fileConfig(fname='avaframe/in3Utils/logging.conf', defaults={'logfilename': logFileName})
-log = logging.getLogger(__name__)
-config_logger = ConfigLogger(log)
-config_logger(cfg)
+# Dump config to log file
+logUtils.dumpCfg(cfg,'com2AB')
 
-#---------------------------------------------
-# Start ALPHABETA
-#---------------------------------------------
-
-#################################
-# Read input data for ALPHABETA #
-# Preprocessing
-#################################
-
-# extract input file locations
+# Extract input file locations
 cfgPath = com2AB.readABinputs(avalancheDir)
 
 log.info("Running com2ABMain model on test case DEM \n %s \n with profile \n %s ",
-         cfgPath['DGMSource'], cfgPath['ProfileLayer'])
+         cfgPath['dgmSource'], cfgPath['profileLayer'])
 
-DGM = com2AB.readRaster(cfgPath['DGMSource'])
-Avapath = com2AB.readAvaPath(cfgPath['ProfileLayer'], cfgPath['DefaltName'], DGM['header'])
-SplitPoint = com2AB.readSplitPoint(cfgPath['SplitPointSource'], DGM['header'])
+# Read input data for ALPHABETA
+dgm = com2AB.readRaster(cfgPath['dgmSource'])
+avaPath = com2AB.readAvaPath(cfgPath['profileLayer'], cfgPath['defaultName'], dgm['header'])
+splitPoint = com2AB.readSplitPoint(cfgPath['splitPointSource'], dgm['header'])
 
-#################################
-# Calculate ALPHABETA #
-# Processing
-#################################
-com2AB.com2ABMain(DGM, Avapath, SplitPoint, cfgPath['saveOutPath'],
+# Calculate ALPHABETA
+com2AB.com2ABMain(dgm, avaPath, splitPoint, cfgPath['saveOutPath'],
                   cfgSetup)
 
 
-#################################
 # Analyse/ plot/ write results #
-# Postprocessing
-#################################
-fileNamePlot_ext, fileNameWrite_ext = outAB.writeABpostOut(DGM,
-                                                           Avapath, SplitPoint,
-                                                           cfgPath['saveOutPath'],
-                                                           cfgFlags)
+plotFile, writeFile = outAB.writeABpostOut(dgm,
+                                           avaPath, splitPoint,
+                                           cfgPath['saveOutPath'],
+                                           cfgFlags)
 
-log.info('Plotted to: %s', fileNamePlot_ext)
-log.info('Data written: %s', fileNameWrite_ext)
+log.info('Plotted to: %s', plotFile)
+log.info('Data written: %s', writeFile)
