cfb2a5930522074f6d1688c9bb8a68c8df6179fe
Matthias Tonnel <matthias@avaframe.org>
2020-08-14 13:18:16 +0200

    typo

--- avaframe/in3Utils/initializeProject.py
+++ avaframe/in3Utils/initializeProject.py
@@ -6,7 +6,7 @@ log = logging.getLogger(__name__)
 
 def initializeFolderStruct(pathAvaName):
     if os.path.exists(pathAvaName):
-        log.info("Running initialization sequecnce for avalanche %s", str(pathAvaName))
+        log.info("Running initialization sequence for avalanche %s", str(pathAvaName))
         log.warning("Folder %s already exists. Checking folder structure and adding missing files and folders.", str(
             pathAvaName).split('/')[-1])
         createFolderStruct(pathAvaName)
@@ -16,7 +16,7 @@ def initializeFolderStruct(pathAvaName):
         except IOError as e:
             print('I/O error({0}): {1}'.format(e.errno, e.strerror))
             return
-        log.info("Running initialization sequecnce for avalanche %s", str(pathAvaName))
+        log.info("Running initialization sequence for avalanche %s", str(pathAvaName))
         createFolderStruct(pathAvaName)
         log.info("Done initializing avalanche %s", str(pathAvaName).split('/')[-1])
 
