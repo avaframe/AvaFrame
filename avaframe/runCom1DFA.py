746cdc5d6a3c4b86ccbb4e75fd93acb0ac890b6c
Anna Wirbel <anna@avaframe.org>
2020-08-14 08:19:54 +0200

    Add com1DFA files

---
+++ avaframe/runCom1DFA.py
@@ -0,0 +1,35 @@
+"""
+    Run script for running Standalone DFA
+    This file is part of Avaframe.
+"""
+
+# Local imports
+from avaframe.com1DFA import com1DFA
+from avaframe.in3Utils import cfgUtils
+from avaframe.in3Utils import logUtils
+
+
+#----------Rewquired Settings---------------
+
+#TODO move this to a main cfg file!
+
+# Avalanche directory; see doc.avaframe.org for setup
+# TODO: full path needed?
+avalancheDir = 'data/avaSlide'
+
+# log file name; leave empty to use default runLog.log
+logName = 'runCom1DFA'
+#------------------------------------------
+
+
+# Start logging
+log = logUtils.initiateLogger(avalancheDir, logName)
+log.info('MAIN SCRIPT')
+log.info('Current avalanche: %s', avalancheDir)
+
+# Load input parameters from configuration file
+[cfg, modPath] = cfgUtils.getModuleConfig(com1DFA, flagPath=True)
+
+
+# Run Standalone DFA
+com1DFA.runSamos(cfg, avalancheDir, modPath)
