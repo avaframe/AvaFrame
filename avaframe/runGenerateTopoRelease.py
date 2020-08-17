f01e6130ade78b9952243f0d2ecfca3b2610f5bd
Anna Wirbel <anna@avaframe.org>
2020-08-17 14:24:31 +0200

    Add simple release area generation

--- avaframe/runGenerateTopoRelease.py
+++ avaframe/runGenerateTopoRelease.py
@@ -7,6 +7,7 @@ import sys
 import logging
 
 # Local imports
+from avaframe.in3Utils import getReleaseArea as gR
 from avaframe.in3Utils import generateTopo as gT
 from avaframe.in3Utils import cfgUtils, logUtils
 from avaframe.out3SimpPlot import outGenerateTopo as oT
@@ -17,10 +18,10 @@ from avaframe.out3SimpPlot import outGenerateTopo as oT
 
 # Avalanche directory; see doc.avaframe.org for setup
 # TODO: full path needed?
-avalancheDir = 'data/avaInclinedPlane'
+avalancheDir = 'data/avaHelix'
 
 # log file name; leave empty to use default runLog.log
-logName = 'generateTopo'
+logName = 'generateTopoGetReleaseArea'
 #------------------------------------------
 
 # Start logging
@@ -28,10 +29,27 @@ log = logUtils.initiateLogger(avalancheDir, logName)
 log.info('MAIN SCRIPT')
 
 # Load input parameters from configuration file
-cfg = cfgUtils.getModuleConfig(gT)
+cfgT = cfgUtils.getModuleConfig(gT)
+cfgR = cfgUtils.getModuleConfig(gR)
 
 # Call main function to generate DEMs
-[z, name_ext, outDir] = gT.generateTopo(cfg, avalancheDir)
+[z, name_ext, outDir] = gT.generateTopo(cfgT, avalancheDir)
 
 # Plot new topogrpahy
-oT.plotDEM(z, name_ext, cfg, outDir)
+oT.plotDEM(z, name_ext, cfgT, outDir)
+
+# Initialise DEM_type
+DEM_type = cfgT['TOPO']['DEM_type']
+
+# Initialise output dir
+outDir = gT.makeOutputDir(avalancheDir)
+
+if DEM_type == 'HX' or DEM_type == 'BL':
+    log.warning('There is no release area available for this DEM type')
+else:
+    # Make release area
+    [xv, yv, xyPoints] = gR.getReleaseArea(cfgT, cfgR, outDir)
+
+    # Plot Release Area
+    if cfgR.getboolean('GENERAL', 'showplot'):
+        oT.plotReleasePoints(xv, yv, xyPoints, DEM_type)
