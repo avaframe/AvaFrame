c1acd9bd649e5563fd77a0e7ef89f068cb90964c
Anna Wirbel <anna@avaframe.org>
2020-08-24 08:45:50 +0200

    Update test functions

--- avaframe/tests/test_generateTopo.py
+++ avaframe/tests/test_generateTopo.py
@@ -13,9 +13,9 @@ import pytest
 import configparser
 
 def test_getParabolaParams():
-    """ Test if A and B is computed correctly """
+    """ Test if parabola parameters are computed correctly """
 
-    # input parameters
+    # Input parameters
     C = 1000.
     fLens = 2250.
     meanAlpha = 0.
@@ -27,6 +27,7 @@ def test_getParabolaParams():
     # Get parabola Parameters
     [A, B, fLen] = gT.getParabolaParams(cfg)
 
+    # Expected results
     ATest1 = 0.00019753086419753085
     BTest1 = -0.8888888888888888
     fLenTest1 = 2250.
@@ -44,10 +45,12 @@ def test_getParabolaParams():
     # Get parabola Parameters
     [A2, B2, fLen2] = gT.getParabolaParams(cfg)
 
+    # Expected results
     ATest2 = 0.0004549617392929703
     BTest2 = -1.3490170336848535
     fLenTest2 = 1482.56096851274
 
+    # Test
     assert A - ATest1 == pytest.approx(0.0, abs=1.e-12)
     assert B - BTest1 == pytest.approx(0.0, abs=1.e-12)
     assert(fLen == fLenTest1)
@@ -65,32 +68,43 @@ def test_getGridDefs():
 
     dx, xEnd, yEnd = gT.getGridDefs(cfg)
 
+    # second Test
+    cfg = configparser.ConfigParser()
+    cfg['TOPO'] = {'dx': 10., 'xEnd': 10., 'yEnd': 100.}
+    dx2, xEnd2, yEnd2 = gT.getGridDefs(cfg)
+
+    # Test
     assert xEnd == pytest.approx(15.0, abs=1.e-12)
     assert yEnd == pytest.approx(105.0, abs=1.e-12)
-    assert yEnd != pytest.approx(100.0, abs=1.e-12)
+    assert xEnd2 != pytest.approx(15.0, abs=1.e-12)
+    assert yEnd2 != pytest.approx(105.0, abs=1.e-12)
+
 
 def test_computeCoordGrid():
     """ Test if vectors that define the domain and raster sizes are
         generated correctly
     """
 
-    xvTest = np.array([0, 1, 2, 3 ,4])
-    yvTest = np.array([0, 1, 2])
-    nColsTest = 5
-    nRowsTest = 3
-
+    # Load function with given inputs
     xv, yv, zv, x, y, nRows, nCols = gT.computeCoordGrid(1., 5., 3.)
 
+    # Define correct results
+    xvTest = np.array([0, 1, 2, 3 ,4])
+    yvTest = np.array([-1.5, -0.5, 0.5])
+
+    # Test
     assert nCols == 5
     assert nRows == 3
+    assert xv[2] == xvTest[2]
+    assert yv[-1] == yvTest[-1]
 
-#
+
 def test_flatplane():
     """ Test flat plane generation """
 
     # Initialise input in correct format
     cfg = configparser.ConfigParser()
-    cfg['TOPO'] = {'zElev': 0.0, 'dx': 5., 'xEnd': 10., 'yEnd': 100.}
+    cfg['TOPO'] = {'zElev': 50.0, 'dx': 5., 'xEnd': 10., 'yEnd': 100.}
 
     # Call function to be tested
     x, y, zv = gT.flatplane(cfg)
@@ -98,43 +112,43 @@ def test_flatplane():
     zMin = np.amin(zv)
     zMean = np.mean(zv)
 
-    assert zMax == pytest.approx(0.0, abs=1.e-12)
-    assert zMin == pytest.approx(0.0, abs=1.e-12)
-    assert(zMean == 0.0)
+    # Test
+    assert zMax == pytest.approx(50.0, abs=1.e-12)
+    assert zMin == pytest.approx(50.0, abs=1.e-12)
+    assert zMean == 50.0
 
 
 def test_inclinedplane():
     """ Test inclined plane generation """
 
+    # Initialise input in correct format
     cfg = configparser.ConfigParser()
     cfg['TOPO'] = {'dx': 5, 'xEnd': 5000., 'yEnd': 2000., 'z0': 2200.,
                'meanAlpha': 34., 'channel': 'False', 'topoconst': 'True'}
     cfg['CHANNELS'] ={'c_ff': 250, 'c_radius': 100}
-    nrows = 301
-    ncols = 1001
 
     # Call function to be tested
     x, y, z = gT.inclinedplane(cfg)
 
-    # Load reference Solution
+    # Load reference solution
     zSol = np.loadtxt(os.path.join(os.getcwd(), 'avaframe/tests/refData/myDEM_IP_Topo.asc'), skiprows=6)
 
     # Compare result to reference solution
     testRes = np.allclose(z, zSol, atol=1.e-6)
 
+    # Test
     assert (testRes == True)
 
 
 def test_hockeysmooth():
     """ Test hockey smooth generation """
 
+    # Initialise input in correct format
     cfg = configparser.ConfigParser()
     cfg['TOPO'] = {'dx': 5, 'xEnd': 5000., 'yEnd': 2000., 'rCirc': 200.,
                'z0' : 2200., 'meanAlpha': 34., 'channel': 'True',
                'narrowing' : 'True', 'topoconst': 'True'}
     cfg['CHANNELS'] ={'c_ff': 250, 'c_radius': 100, 'c_init' : 250, 'c_mustart' : 0.2, 'c_muendFP' : 0.86}
-    nrows = 301
-    ncols = 1001
 
     # Call function to be tested
     x, y, z = gT.hockeysmooth(cfg)
@@ -145,18 +159,18 @@ def test_hockeysmooth():
     # Compare result to reference solution
     testRes = np.allclose(z, zSol, atol=5.e-6)
 
+    Test
     assert (testRes == True)
 
 
 def test_hockey():
     """ Test hockey generation """
 
+    # Initialise input in correct format
     cfg = configparser.ConfigParser()
     cfg['TOPO'] = {'C' : 1000, 'meanAlpha' : 34, 'fLens' : 0, 'dx': 5, 'xEnd': 5000.,
                   'yEnd': 2000., 'channel': 'False', 'topoconst': 'True'}
     cfg['CHANNELS'] ={'c_ff': 250, 'c_radius': 100, 'c_mustart' : 0.2, 'c_muend' : 0.6, 'c_init' : 250}
-    nrows = 301
-    ncols = 1001
 
     # Call function to be tested
     x, y, z = gT.hockey(cfg)
@@ -167,18 +181,18 @@ def test_hockey():
     # Compare result to reference solution
     testRes = np.allclose(z, zSol, atol=1.e-6)
 
+    # Test
     assert (testRes == True)
 
 
 def test_helix():
     """ Test helix generation """
 
+    # Initialise input in correct format
     cfg = configparser.ConfigParser()
     cfg['TOPO'] = {'C' : 1000, 'meanAlpha' : 34, 'rHelix' : 1250, 'fLens' : 0, 'dx': 5, 'xEnd': 5000.,
                   'yEnd': 2000.,  'channel': 'True', 'narrowing' : 'True', 'topoconst': 'True'}
     cfg['CHANNELS'] ={'c_ff': 250, 'c_radius': 100, 'c_mustart' : 0.2, 'c_muend' : 0.6, 'c_init' : 250}
-    nrows = 301
-    ncols = 1001
 
     # Call function to be tested
     x, y, z = gT.helix(cfg)
@@ -189,4 +203,5 @@ def test_helix():
     # Compare result to reference solution
     testRes = np.allclose(z, zSol, atol=1.e-6)
 
+    # Test
     assert (testRes == True)
