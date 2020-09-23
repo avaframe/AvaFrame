2633d3373cef37a4d925289ff4d5aca4e84e6c35
Anna Wirbel <anna@avaframe.org>
2020-09-23 15:54:12 +0200

    Add simple plot

---
+++ avaframe/runQuickPlotSimple.py
@@ -0,0 +1,28 @@
+"""
+    Run script for plotting two matrix datasets
+    This file is part of Avaframe.
+"""
+
+# Load modules
+import os
+
+# Local imports
+from avaframe.out3SimpPlot import outQuickPlotSimple
+from avaframe.in3Utils import cfgUtils
+from avaframe.in3Utils import logUtils
+
+# log file name; leave empty to use default runLog.log
+logName = 'runQuickPlot'
+
+# Load avalanche directory from general configuration file
+cfgMain = cfgUtils.getGeneralConfig()
+avalancheDir = cfgMain['MAIN']['avalancheDir']
+
+# Start logging
+log = logUtils.initiateLogger(avalancheDir, logName)
+log.info('MAIN SCRIPT')
+log.info('Current avalanche: %s', avalancheDir)
+
+# Set directory where input files is located
+inputDir = os.path.join(avalancheDir, 'Work', 'simplePlot')
+outQuickPlotSimple.quickPlotSimple(avalancheDir, inputDir)
