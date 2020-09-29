01e9cb24fc8ccf130c1f5e1bcdbb212ebab4baee
Felix Oesterle <felix@avaframe.org>
2020-09-29 22:14:11 +0200

    Add function to Aimec to add/style/label colorbar

--- avaframe/out3Plot/plotUtils.py
+++ avaframe/out3Plot/plotUtils.py
@@ -39,7 +39,6 @@ markers = cfg['markerStyle']
 ms = float(cfg['markerSize'])
 matplotlib.rcParams['lines.linewidth'] = float(cfg['lineWidth'])
 matplotlib.rcParams['lines.markersize'] = ms
-colorbarOutline = False
 # font size
 fs = float(cfg['fontSize'])
 matplotlib.rcParams['figure.titlesize'] = cfg['titleSize']
