1356e0269d6152efec8a3edd211790c9e9e03e88
Matthias Tonnel <matthias@avaframe.org>
2020-10-20 13:27:26 +0200

    update AB tests

--- avaframe/tests/test_com2AB.py
+++ avaframe/tests/test_com2AB.py
@@ -126,42 +126,30 @@ def test_calcAB(capfd):
 def test_com2ABMain(capfd):
     '''Simple test for function com2ABMain'''
     # load and prepare Inputs
-    listNames = ['avaHockeySmoothSmall', 'avaHockeySmoothChannel',
-                 'avaBowl', 'avaHelix']
+    listNames = ['avaHockeySmoothSmall', 'avaHockeySmoothChannel', 'avaBowl']
     dirname = os.path.dirname(__file__)
     for name in listNames:
         avalancheDir = os.path.join(dirname, '..', 'data', name)
-        saveOutPath = os.path.join(dirname, 'data')
         saveOutPathRef = os.path.join(dirname, '..', '..', 'benchmarks', name)
         cfg = cfgUtils.getModuleConfig(com2AB)
-        cfgSetup = cfg['ABSETUP']
         flags = cfg['FLAGS']
-
-        # Extract input file locations
-        cfgPath = com2AB.readABinputs(avalancheDir)
-
-        # Read input data for ALPHABETA
-        dem = IOf.readRaster(cfgPath['demSource'])
-        avaPath = shpConv.readLine(cfgPath['profileLayer'],
-                                   cfgPath['defaultName'], dem)
-        splitPoint = shpConv.readPoints(cfgPath['splitPointSource'], dem)
         # run main routine
-        com2AB.com2ABMain(dem, avaPath, splitPoint, saveOutPath, cfgSetup)
-        eqParams, eqOut = outAB.readABresults(saveOutPath, avaPath['Name'][0],
-                                              flags)
+        resAB = com2AB.com2ABMain(cfg, avalancheDir)
+        eqParams = resAB['eqParams']
+        eqOut = resAB[name]
+
         # open ref data
         flags['fullOut'] = 'True'
-        eqParamsRef, eqOutRef = outAB.readABresults(saveOutPathRef,
-                                                    avaPath['Name'][0], flags)
+        eqParamsRef, eqOutRef = outAB.readABresults(saveOutPathRef, name, flags)
 
         for key in eqParamsRef.keys():
             assert eqParamsRef[key] == eqParams[key]
 
         atol = 1e-10
-        assert (np.allclose(eqOutRef['x'], eqOutRef['x'], atol=atol)) and (
-                np.allclose(eqOutRef['y'], eqOutRef['y'], atol=atol)) and (
-                np.allclose(eqOutRef['z'], eqOutRef['z'], atol=atol)) and (
-                np.allclose(eqOutRef['s'], eqOutRef['s'], atol=atol)) and (
-                np.allclose(eqOutRef['alpha'], eqOutRef['alpha'], atol=atol)
-                ) and (np.allclose(eqOutRef['alphaSD'], eqOutRef['alphaSD'],
+        assert (np.allclose(eqOutRef['x'], eqOut['x'], atol=atol)) and (
+                np.allclose(eqOutRef['y'], eqOut['y'], atol=atol)) and (
+                np.allclose(eqOutRef['z'], eqOut['z'], atol=atol)) and (
+                np.allclose(eqOutRef['s'], eqOut['s'], atol=atol)) and (
+                np.allclose(eqOutRef['alpha'], eqOut['alpha'], atol=atol)
+                ) and (np.allclose(eqOutRef['alphaSD'], eqOut['alphaSD'],
                        atol=atol))
