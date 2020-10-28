a16021136dd327598bb3cc81afdcca96c1a490f8
matthiasto <matthias@avaframe.org>
2020-10-28 17:24:04 +0100

    tools

--- avaframe/DFAkernel/test.py
+++ avaframe/DFAkernel/test.py
@@ -27,13 +27,63 @@ if __name__ == "__main__":
     log.info('MAIN SCRIPT')
     log.info('Current avalanche: %s', avalancheDir)
 
+    # fetch input data
     inputDir = os.path.join(avalancheDir, 'Inputs')
     relFiles = glob.glob(inputDir+os.sep + 'REL'+os.sep + '*.shp')
     demFile = glob.glob(inputDir+os.sep+'*.asc')
     dem = IOf.readRaster(demFile[0])
     releaseLine = shpConv.readLine(relFiles[0], 'release1', dem)
-    relRaster = tools.getRelease(dem['header'], releaseLine)
+
+    # process release to get it as a raster
+    relRaster = tools.polygon2Raster(dem['header'], releaseLine)
     relTh = 1
     relRasterD = relRaster * relTh
 
+    # initialize simulation : create particles
     particles = tools.initializeRelease(relRaster, dem)
+
+    # get normal vector of the grid mesh
+    Nx, Ny, Nz = tools.getNormalVect(dem['rasterData'], dem['header'].cellsize)
+    mesh = {}
+    # mesh['header'] = dem['header']
+    mesh['Nx'] = Nx
+    mesh['Ny'] = Ny
+    mesh['Nz'] = Nz
+
+    Npart = particles['Npart']
+    csz = dem['header'].cellsize
+    # loop on particles
+    for i in range(Npart):
+        m = particles['m'][i]
+        x = particles['x'][i]
+        y = particles['y'][i]
+        z = particles['z'][i]
+        d = particles['d'][i]
+        ux = particles['ux'][i]
+        uy = particles['uy'][i]
+        uz = particles['uz'][i]
+        # get normal at the particle location
+        Point = {}
+        Point['x'] = x
+        Point['y'] = y
+
+        nx = geoTrans.projectOnRasterRoot(x, y, Nx, csz=csz)
+        ny = geoTrans.projectOnRasterRoot(x, y, Ny, csz=csz)
+        nz = geoTrans.projectOnRasterRoot(x, y, Nz, csz=csz)
+        nx, ny, nz = tools.normalize(nx, ny, nz)
+
+        xEnd = x + dt * ux
+        yEnd = y + dt * uy
+        nxEnd = geoTrans.projectOnRasterRoot(xEnd, yEnd, Nx, csz=csz)
+        nyEnd = geoTrans.projectOnRasterRoot(xEnd, yEnd, Ny, csz=csz)
+        nzEnd = geoTrans.projectOnRasterRoot(xEnd, yEnd, Nz, csz=csz)
+        nxEnd, nyEnd, nzEnd = tools.normalize(nxEnd, nyEnd, nzEnd)
+
+        nxAvg = nx + nxEnd
+        nyAvg = ny + nyEnd
+        nzAvg = nz + nzEnd
+        nxAvg, nyAvg, nzAvg = tools.normalize(nxAvg, nyAvg, nzAvg)
+
+        accxNormCurv = - ux * (nxEnd-nx) / dt
+        accyNormCurv = - uy * (nyEnd-ny) / dt
+        acczNormCurv = - uz * (nzEnd-nz) / dt
