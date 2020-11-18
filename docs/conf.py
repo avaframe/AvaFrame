4c7a4d4414dbd6a6d5008287ed565fd9163429a3
Felix Oesterle <felix@avaframe.org>
2020-11-18 20:02:22 +0100

    Add sphinx auto doc to sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
@@ -30,7 +31,8 @@ author = 'AvaFrame developers'
 extensions = [
     'sphinx_rtd_theme',
     'sphinx.ext.autosectionlabel',
-    'sphinxcontrib.bibtex'
+    'sphinxcontrib.bibtex',
+    'sphinx.ext.autodoc'
 ]
 autosectionlabel_prefix_document = True
 # Add any paths that contain templates here, relative to this directory.
@@ -39,7 +41,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*run*']
 
 # Set the master document name for readthedocs builds
 master_doc = 'index'
