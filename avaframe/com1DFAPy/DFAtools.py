fixing the calculation of the area and SPH

avaframe/com1DFAPy/DFAtools.py
@@ -182,13 +182,28 @@ def getNormalMesh(z, csz, num=4):
         Ny[n-1, m-1] = (z[n-2, m-1] - z[n-1, m-1] + z[n-2, m-2] - z[n-1, m-2]) / csz
         Nz[n-1, m-1] = 2
 
+    if num == 1:
+        z1 = np.append(z, z[:, -2].reshape(n, 1), axis=1)
+        n1, m1 = np.shape(z1)
+        z2 = np.append(z1, z1[-2, :].reshape(1, m1), axis=0)
+        n2, m2 = np.shape(z2)
+        Nx = - (z2[0:n2-1, 1:m2] - z2[0:n2-1, 0:m2-1]) / csz
+        Ny = - (z2[1:n2, 0:m2-1] - z2[0:n2-1, 0:m2-1]) / csz
+        Ny[n-1, 0:m-1] = -Ny[n-1, 0:m-1]
+        Nx[0:n-1, m-1] = -Nx[0:n-1, m-1]
+        Ny[n-1, m-1] = -Ny[n-1, m-1]
+        Nx[n-1, m-1] = -Nx[n-1, m-1]
+
+        # Nx[0:n-1, 0:m-1] = - (z[0:n-1, 1:m] - z[0:n-1, 0:m-1]) / csz
+        # Ny[0:n-1, 0:m-1] = - (z[1:n, 0:m-1] - z[0:n-1, 0:m-1]) / csz
+
     Nx, Ny, Nz = normalize(Nx, Ny, Nz)
 
     return Nx, Ny, Nz
 
 
 def getAreaMesh(Nx, Ny, Nz, csz):
-    A = 1/(Nz*Nz) * np.sqrt((Nz*Nz + Nx*Nx) * (Nz*Nz + Ny*Ny)) * csz*csz
+    A = csz * csz / Nz
     A = np.where(A > 3*csz*csz, 3*csz*csz, A)
     return A
 

avaframe/com1DFAPy/SPHfunctions.py
@@ -158,7 +158,7 @@ def calcGradHSPH(particles, j, ncols, nrows, csz):
     return gradhX, gradhY,  gradhZ, L
 
 
-def calcGradHSPHVect(particles, j, ncols, nrows, csz):
+def calcGradHSPHVect(particles, j, ncols, nrows, csz, nx, ny, nz):
     # SPH kernel
     # use "spiky" kernel: w = (h - r)**3 * 10/(pi*h**5)
     rKernel = csz
@@ -204,6 +204,10 @@ def calcGradHSPHVect(particles, j, ncols, nrows, csz):
     dx = particles['x'][L] - x
     dy = particles['y'][L] - y
     dz = (particles['z'][L] - z)
+    dn = nx*dx + ny*dy + nz*dz
+    dx = dx - dn*nx
+    dy = dy - dn*ny
+    dz = dz - dn*nz
     r = DFAtls.norm(dx, dy, dz)
     # impose a minimum distance between particles
     dx = np.where(r < 0.0001 * rKernel, 0.0001 * rKernel * dx, dx)
@@ -331,6 +335,9 @@ def computeFD(cfg, particles, dem):
     nrows = dem['header'].nrows
     ncols = dem['header'].ncols
     csz = dem['header'].cellsize
+    Nx = dem['Nx']
+    Ny = dem['Ny']
+    Nz = dem['Nz']
     # initialize
     H = np.zeros(np.shape(particles['h']))
     # loop on particles
@@ -340,7 +347,13 @@ def computeFD(cfg, particles, dem):
         # adding lateral force (SPH component)
         # startTime = time.time()
         # gradhX, gradhY,  gradhZ, _ = calcGradHSPH(particles, j, ncols, nrows, csz)
-        h, _ = calcHSPHVect(particles, j, ncols, nrows, csz)
+
+        # get normal at the particle location
+        x = particles['x'][j]
+        y = particles['y'][j]
+        nx, ny, nz = DFAtls.getNormal(x, y, Nx, Ny, Nz, csz)
+
+        h, _ = calcHSPHVect(particles, j, ncols, nrows, csz, nx, ny, nz)
         H[j] = h / rho
 
     particles['hSPH'] = H
@@ -348,7 +361,7 @@ def computeFD(cfg, particles, dem):
     return particles
 
 
-def calcHSPHVect(particles, j, ncols, nrows, csz):
+def calcHSPHVect(particles, j, ncols, nrows, csz, nx, ny, nz):
     # SPH kernel
     # use "spiky" kernel: w = (h - r)**3 * 10/(pi*h**5)
     rKernel = csz
@@ -391,6 +404,10 @@ def calcHSPHVect(particles, j, ncols, nrows, csz):
     dx = particles['x'][L] - x
     dy = particles['y'][L] - y
     dz = (particles['z'][L] - z)
+    dn = nx*dx + ny*dy + nz*dz
+    dx = dx - dn*nx
+    dy = dy - dn*ny
+    dz = dz - dn*nz
     r = DFAtls.norm(dx, dy, dz)
     # impose a minimum distance between particles
     r = np.where(r < 0.0001 * rKernel, 0.0001 * rKernel, r)