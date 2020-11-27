e8549fe02fb2c3391be41d308d357dc074331c66
Felix Oesterle <felix@avaframe.org>
2020-11-27 10:22:40 +0100

    Remove import in doc conf (rtd fails)

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import sys
 import avaframe
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../avaframe/'))
+# sys.path.insert(0, os.path.abspath('../avaframe/'))
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
