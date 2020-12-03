a4be21158f5cd441a062df44bbfe2fd834a3346c
matthiasto <matthias@avaframe.org>
2020-12-03 10:21:28 +0100

    exted test domain

--- avaframe/com1DFAPy/SPHfunctions.py
+++ avaframe/com1DFAPy/SPHfunctions.py
@@ -366,6 +366,7 @@ def calcGradHSPHVect(particles, j, ncols, nrows, csz, nx, ny, nz):
         MM1[0, 1] = MM1[0, 1] - M[2, 0]*M[1, 2]/M[2, 2]
         MM1[1, 0] = MM1[1, 0] - M[2, 1]*M[0, 2]/M[2, 2]
         MM1[1, 1] = MM1[1, 1] - M[2, 1]*M[1, 2]/M[2, 2]
+        # print(M)
 
         # buil the matrix that transforms the gradient in (r1, r2, r3) to the one in (x1, x2, x3)
         MMGrad = MM1.T
