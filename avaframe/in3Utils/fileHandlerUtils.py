306764d48b1e259d7c3e42edeb89e7a569108848
Anna Wirbel <anna@avaframe.org>
2021-01-14 11:29:52 +0100

    Add plot function

--- avaframe/in3Utils/fileHandlerUtils.py
+++ avaframe/in3Utils/fileHandlerUtils.py
@@ -312,13 +312,10 @@ def exportcom1DFAOutput(avaDir, cfg='', addTSteps=False):
                                     '%s_tLast_fd.txt' % logDict['simName'][k])
             pathTo = os.path.join(outDirPF, 'timeSteps', '%s_%.05f_tLast_fd.asc' % (logDict['simName'][k], logDict[varPar][k]))
             shutil.copy(pathFrom, pathTo)
-            print('export Last', pathFrom, pathTo)
             pathFrom = os.path.join('%s%.05f' % (resPath, logDict[varPar][k]),
                                     '%s_tFirst_fd.txt' % logDict['simName'][k])
             pathTo = os.path.join(outDirPF, 'timeSteps', '%s_%.05f_tFirst_fd.asc' % (logDict['simName'][k], logDict[varPar][k]))
             shutil.copy(pathFrom, pathTo)
-            print('export First', pathFrom, pathTo)
-
 
     # Export ExpLog to Outputs/com1DFA
     shutil.copy2(os.path.join('%s' % inputDir, 'ExpLog.txt'), outDir)
