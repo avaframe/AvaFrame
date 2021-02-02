c44a7d0263e57189dbb81448d1b6321b7d0a6290
Felix Oesterle <felix@avaframe.org>
2021-02-03 00:15:35 +0100

    add and modify DEM plot functions, add get/readDEM functions
        addresses #292 and #293

--- avaframe/out3Plot/outTopo.py
+++ avaframe/out3Plot/outTopo.py
@@ -1,7 +1,5 @@
 """
-    Simple plotting for idealised/generic DEM results
-
-    This file is part of Avaframe.
+    Simple plotting for DEMs
 """
 
 import matplotlib.pyplot as plt
@@ -10,8 +8,48 @@ import os
 import numpy as np
 import logging
 
+# local imports
+from avaframe.in1Data import getInput
+
 # create local logger
 log = logging.getLogger(__name__)
+
+def _generateDEMPlot(X, Y, z,):
+    """Generates 3d DEM plot, use this to style the plot"""
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(X, Y, z, cmap=plt.cm.viridis,
+                    linewidth=0, antialiased=False)
+    return ax
+
+def plotDEM(cfg):
+    """Plots the DEM from the avalancheDir in cfg alongside it
+
+    Parameters
+    ----------
+    cfg : configparser object
+      the main configuration
+
+    Returns
+    -------
+    plt : matplotlib object
+      plot object in case displaying is needed
+    """
+
+    # get avalanche dir
+    avalancheDir = cfg['MAIN']['avalancheDir']
+    log.info('Plotting DEM in : %s', avalancheDir)
+
+    # read DEM
+    dem = getInput.readDEM(avalancheDir)
+
+    header = dem['header']
+    xl = header.xllcenter
+    yl = header.yllcenter
+    dx = header.cellsize
+    z = dem['rasterData']
+
+    # Set coordinate grid with given origin
+    X,Y = _setCoordinateGrid(xl,yl,dx,z)
 
 def plotGeneratedDEM(z, nameExt, cfg, outDir):
     """ Plot DEM with given information on the origin of the DEM """
@@ -24,20 +62,14 @@ def plotGeneratedDEM(z, nameExt, cfg, outDir):
     xl = float(cfgDEM['xl'])
     yl = float(cfgDEM['yl'])
     demName = cfgDEM['demName']
-    xEnd = z.shape[1] * dx
-    yEnd = z.shape[0] * dx
 
     # Set coordinate grid with given origin
-    xp = np.arange(xl, xl + xEnd, dx)
-    yp = np.arange(yl, yl + yEnd, dx)
-    X, Y = np.meshgrid(xp, yp)
+    X,Y = _setCoordinateGrid(xl,yl,dx,z)
 
     topoNames = {'IP': 'inclined Plane', 'FP': 'flat plane', 'HS': 'Hockeystick',
                  'HS2': 'Hockeystick smoothed', 'BL': 'bowl', 'HX': 'Helix', 'PY': 'Pyramid'}
 
-    ax = plt.axes(projection='3d')
-    ax.plot_surface(X, Y, z, cmap=plt.cm.viridis,
-                    linewidth=0, antialiased=False)
+    ax = _generateDEMPlot(X, Y, z)
 
     ax.set_title('Generated DEM: %s' % (topoNames[nameExt]))
     ax.set_xlabel('along valley distance [m]')
@@ -71,3 +103,15 @@ def plotReleasePoints(xv, yv, xyPoints, demType):
     plt.ylabel('across valley [m]')
 
     plt.show()
+
+
+def _setCoordinateGrid(xl,yl,dx,z):
+    """get a Coordinate Grid for plotting"""
+
+    xEnd = z.shape[1] * dx
+    yEnd = z.shape[0] * dx
+
+    xp = np.arange(xl, xl + xEnd, dx)
+    yp = np.arange(yl, yl + yEnd, dx)
+    X, Y = np.meshgrid(xp, yp)
+    return(X,Y)
