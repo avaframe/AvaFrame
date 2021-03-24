a10c72b21fe0513ee244f59592598a645012b703
Felix Oesterle <felix@avaframe.org>
2021-03-24 21:22:48 +0100

    Modify setup.py to use scm setuptools, include MANIFEST

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Setup file for the avaframe package.
-   Adapted from the Python Packaging Authority template."""
+   Adapted from the Python Packaging Authority template.
+   See MANIFEST.in for non-python files (include an exclude)
+
+"""
 
 from setuptools import setup, find_packages  # Always prefer setuptools
 
@@ -10,6 +13,9 @@ with open('README.md') as f:
 with open('LICENSE.txt') as f:
     license = f.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 DISTNAME = 'avaframe'
 LICENSE = 'EUPL'
 AUTHOR = 'AvaFrame Contributors'
@@ -18,45 +24,54 @@ URL = 'http://avaframe.org'
 CLASSIFIERS = [
         # How mature is this project? Common values are
         # 3 - Alpha  4 - Beta  5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: EUPL License',
+        # 'License :: OSI Approved :: EUPL License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ]
 
 DESCRIPTION = 'The Open Avalanche Framework'
 
 LONG_DESCRIPTION = readme
 
-req_packages = ['numpy',
+req_packages = ['pyshp',
+                'seaborn',
+                'cmocean'
                 ]
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+def local_scheme(version):
+    return ""
 
 setup(
     # Project info
     name=DISTNAME,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    version='dev',
+    version='0.2.4',
     # The project's main homepage.
     url=URL,
     # Author details
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
+    use_scm_version={"local_scheme": local_scheme},
+    # use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+
     # License
     license=LICENSE,
     classifiers=CLASSIFIERS,
     # We are a python 3 only shop
     python_requires='>=3.5',
     # Find packages automatically
-    packages=find_packages(exclude=['docs']),
+    packages=find_packages(exclude=['docs','benchmarks']),
     # Include package data
     include_package_data=True,
     # Install dependencies
-    install_requires=req_packages,
+    # install_requires=req_packages,
+    install_requires=requirements,
     # additional groups of dependencies here (e.g. development dependencies).
     extras_require={},
     # Executable scripts
