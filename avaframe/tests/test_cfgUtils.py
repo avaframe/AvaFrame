8f17add00abdc6f39d64d28782d476c06ecd368e
Felix Oesterle <felix@avaframe.org>
2021-05-05 21:46:40 +0200

    Add cfgHash test; clean code

--- avaframe/tests/test_cfgUtils.py
+++ avaframe/tests/test_cfgUtils.py
@@ -1,13 +1,14 @@
-"""Tests for module logUtilis"""
+"""Tests for module cfgUtils"""
+
 from avaframe.in3Utils import cfgUtils
 from avaframe.tests import test_logUtils
 import os
 
 
-def test_getModuleConfig(capfd):
-    '''Simple test for module getModuleConfig'''
-    dirname = os.path.dirname(__file__)
-    avalancheDir = dirname
+def test_getModuleConfig():
+    '''Test for module getModuleConfig'''
+
+    avalancheDir = os.path.dirname(__file__)
     # test with both a default and local .ini file
     cfg = cfgUtils.getModuleConfig(test_logUtils)
     sections = cfg.sections()
@@ -21,8 +22,8 @@ def test_getModuleConfig(capfd):
     assert hasKey == False
 
     # test reading a different file
-    filemane = os.path.join(avalancheDir,'local_test_logUtilsCfg.ini')
-    cfg = cfgUtils.getModuleConfig(test_logUtils, fileOverride=filemane)
+    filename = os.path.join(avalancheDir, 'local_test_logUtilsCfg.ini')
+    cfg = cfgUtils.getModuleConfig(test_logUtils, fileOverride=filename)
     sections = cfg.sections()
     assert sections == ['GENERAL', 'FLAGS', 'GOODSECTION1', 'BADSECTION1']
     assert cfg['GENERAL']['inputDir'] == 'path/to/avalanche'
@@ -32,3 +33,23 @@ def test_getModuleConfig(capfd):
     assert cfg['GOODSECTION1']['badKey1'] == 'Bonjour'
     assert cfg['BADSECTION1']['badKey2'] == '10'
     assert cfg['BADSECTION1']['badKey3'] == 'None'
+
+
+def test_cfgHash():
+    '''Test for the uid hash generation '''
+
+    avalancheDir = os.path.dirname(__file__)
+
+    filename = os.path.join(avalancheDir, 'local_test_logUtilsCfg.ini')
+    cfg = cfgUtils.getModuleConfig(test_logUtils, fileOverride=filename)
+
+    uid = cfgUtils.cfgHash(cfg)
+
+    # test for the correct uid
+    assert uid == 'aec6a993be'
+
+    # change and test again
+    cfg['GOODSECTION1']['goodKey1'] = '1.5'
+    uid = cfgUtils.cfgHash(cfg)
+    # make sure it is not the same hash
+    assert uid != 'aec6a993be'
