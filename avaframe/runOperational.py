84ee7a1242ba29378807773a638b2f5cede08524
Felix Oesterle <felix@avaframe.org>
2021-05-20 21:25:40 +0200

    Make runOperational callable; lots commented, working commit

--- avaframe/runOperational.py
+++ avaframe/runOperational.py
@@ -1,71 +1,80 @@
 """
     Run script for running the operational workflow
-    This file is part of Avaframe.
 """
 
 # Load modules
-import os
 import time
 import logging
 
-# Local imports
+# # Local imports
 from avaframe.com1DFA import com1DFA
 from avaframe.com2AB import com2AB
-from avaframe.log2Report import generateReport as gR
-from avaframe.out1Peak import outPlotAllPeak as oP
-from avaframe.out3Plot import outAB
+# # from avaframe.log2Report import generateReport as gR
+# from avaframe.out1Peak import outPlotAllPeak as oP
+# from avaframe.out3Plot import outAB
 from avaframe.in3Utils import cfgUtils
 from avaframe.in3Utils import logUtils
 from avaframe.in3Utils import initializeProject as initProj
 
-# Time the whole routine
-startTime = time.time()
 
-# log file name; leave empty to use default runLog.log
-logName = 'runOperational'
 
-# Load avalanche directory from general configuration file
-# v0.1 -> one avalanche is fine,
-# v0.2 -> if given dir contains more than one avalancheDir -> run loop
-cfgMain = cfgUtils.getGeneralConfig()
-avalancheDir = cfgMain['MAIN']['avalancheDir']
+def runOperational(avalancheDir=''):
+    # Time the whole routine
+    startTime = time.time()
+    print('runOperational')
 
-# Start logging
-log = logUtils.initiateLogger(avalancheDir, logName)
-log.info('MAIN SCRIPT')
-log.info('Current avalanche: %s', avalancheDir)
+    # log file name; leave empty to use default runLog.log
+    logName = 'runOperational'
 
-# ----------------
-# Load input parameters from configuration files
+    # # Load avalanche directory from general configuration file
+    cfgMain = cfgUtils.getGeneralConfig()
+    if avalancheDir != '':
+        cfgMain['MAIN']['avalancheDir'] = avalancheDir
+    else:
+        avalancheDir = cfgMain['MAIN']['avalancheDir']
 
-# ----------------
-# Clean input directory(ies) of old work and output files
-initProj.cleanSingleAvaDir(avalancheDir, keep=logName)
 
-# ----------------
-# Run dense flow
-cfg = cfgUtils.getModuleConfig(com1DFA)
-reportDictList = com1DFA.com1DFAMain(cfg, avalancheDir)
+    print(avalancheDir)
 
-# ----------------
-# Run Alpha Beta
-cfgAB = cfgUtils.getModuleConfig(com2AB)
-resAB = com2AB.com2ABMain(cfgAB, avalancheDir)
+    # Start logging
+    log = logUtils.initiateLogger(avalancheDir, logName)
+    log.info('MAIN SCRIPT')
+    log.info('Current avalanche: %s', avalancheDir)
 
-# ----------------
-# Collect results/plots/report  to a single directory
-# make simple plots (com1DFA, com2AB)
-# peak file plot
+    # ----------------
+    # Load input parameters from configuration files
 
-# Generata plots for all peakFiles
-plotDict = oP.plotAllPeakFields(avalancheDir, cfg, cfgMain['FLAGS'])
-reportDictList, _, _ = outAB.writeABpostOut(resAB, cfgAB, reportDictList)
+    # ----------------
+    # Clean input directory(ies) of old work and output files
+    initProj.cleanSingleAvaDir(avalancheDir, keep=logName)
 
-# Set directory for report
-reportDir = os.path.join(avalancheDir, 'Outputs')
-# write report
-gR.writeReport(reportDir, reportDictList, cfgMain['FLAGS'], plotDict)
+    # # ----------------
+    # # Run dense flow
+    # cfg = cfgUtils.getModuleConfig(com1DFA)
+    # reportDictList = com1DFA.com1DFAMain(cfg, avalancheDir)
 
-# Print time needed
-endTime = time.time()
-log.info('Took %s seconds to calculate.' % (endTime - startTime))
+    # ----------------
+    # Run Alpha Beta
+    cfgAB = cfgUtils.getModuleConfig(com2AB)
+    resAB = com2AB.com2ABMain(cfgAB, avalancheDir)
+
+    # # ----------------
+    # # Collect results/plots/report  to a single directory
+    # # make simple plots (com1DFA, com2AB)
+    # # peak file plot
+
+    # # Generata plots for all peakFiles
+    # plotDict = oP.plotAllPeakFields(avalancheDir, cfg, cfgMain['FLAGS'])
+    # reportDictList, _, _ = outAB.writeABpostOut(resAB, cfgAB, reportDictList)
+
+    # # Set directory for report
+    # reportDir = os.path.join(avalancheDir, 'Outputs')
+    # # write report
+    # gR.writeReport(reportDir, reportDictList, cfgMain['FLAGS'], plotDict)
+
+    # Print time needed
+    endTime = time.time()
+    # log.info('Took %s seconds to calculate.' % (endTime - startTime))
+
+if __name__ == '__main__':
+    runOperational()
