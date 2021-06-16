a37cc0f854f56f852c3162b2a58ab6fb85b0b7f7
Felix Oesterle <felix@avaframe.org>
2021-06-16 22:09:17 +0200

    Get base functionality of runOperational running

--- avaframe/runOperational.py
+++ avaframe/runOperational.py
@@ -6,9 +6,10 @@
 import time
 import os
 import logging
+import pathlib
 
 # # Local imports
-from avaframe.com1DFA import com1DFA
+from avaframe import runCom1DFA
 from avaframe.com2AB import com2AB
 from avaframe.log2Report import generateReport as gR
 from avaframe.out1Peak import outPlotAllPeak as oP
@@ -16,6 +17,7 @@ from avaframe.out3Plot import outAB
 from avaframe.in3Utils import cfgUtils
 from avaframe.in3Utils import logUtils
 from avaframe.in3Utils import initializeProject as initProj
+from avaframe.in3Utils import fileHandlerUtils as fU
 
 
 
@@ -34,26 +36,24 @@ def runOperational(avalancheDir=''):
     else:
         avalancheDir = cfgMain['MAIN']['avalancheDir']
 
-
-    print(avalancheDir)
-
     # Start logging
     log = logUtils.initiateLogger(avalancheDir, logName)
     log.info('MAIN SCRIPT')
     log.info('Current avalanche: %s', avalancheDir)
 
     # ----------------
-    # Load input parameters from configuration files
-
-    # ----------------
     # Clean input directory(ies) of old work and output files
     initProj.cleanSingleAvaDir(avalancheDir, keep=logName)
 
-    # # ----------------
-    # # Run dense flow
-    cfg = cfgUtils.getModuleConfig(com1DFA)
-    reportDictList = com1DFA.com1DFAMain(cfg, avalancheDir)
-    pprFile = '/home/felix/tmp/TestAva2/Outputs/com1DFA/peakFiles/slideRelease_null_dfa_0.15500_ppr.asc'
+    # ----------------
+    # Run dense flow
+    _, _, _, _, _, reportDictList = runCom1DFA.runCom1DFA(avalancheDir)
+
+    # Get peakfiles to return to QGIS
+    avaDir = pathlib.Path(avalancheDir)
+    inputDir = avaDir / 'Outputs' / 'com1DFA' / 'peakFiles'
+    peakFiles = fU.makeSimDict(inputDir, '', avaDir)
+    # print(peakFiles['files'])
 
     # ----------------
     # Run Alpha Beta
@@ -78,9 +78,9 @@ def runOperational(avalancheDir=''):
 
     # Print time needed
     endTime = time.time()
+    log.info('Took %6.1f seconds to calculate.' % (endTime - startTime))
 
-    return str(abShpFile), pprFile
-    # log.info('Took %s seconds to calculate.' % (endTime - startTime))
+    return str(abShpFile), peakFiles['files'] 
 
 if __name__ == '__main__':
     runOperational()
