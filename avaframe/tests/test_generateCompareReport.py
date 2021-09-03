22b7fbaf957cd9838ce8acbd4235f68f07b13ef9
Anna Wirbel <anna@avaframe.org>
2021-09-03 11:10:38 +0200

    add report Tests

---
+++ avaframe/tests/test_generateCompareReport.py
@@ -0,0 +1,149 @@
+"""
+    Pytest for generate Report
+
+    This file is part of Avaframe.
+
+ """
+
+#  Load modules
+import numpy as np
+import os
+from avaframe.log2Report import generateCompareReport as gR
+
+import pytest
+import configparser
+import pathlib
+
+
+def test_makeLists():
+    """ test creating lists for table creation """
+
+    # setup required input
+    simDict = {'type': 'list', 'maxP': '100.', 'minF': 0.0, 'testKey': 10., 'testIgnore': ''}
+    benchDict = {'type': 'list', 'maxP': '200.', 'minF': 0.0, 'testIgnore2': 'A'}
+
+    # call function to be tested
+    parameterList, valuesSim, valuesBench = gR.makeLists(simDict, benchDict)
+
+    assert parameterList == ['maxP', 'minF', 'testKey']
+    assert valuesSim == ['100.', 0.0, 10.]
+    assert valuesBench == ['200.', 0.0, 'non existent']
+
+
+def test_copyQuickPlots(tmp_path):
+    """ test copy plots """
+
+    # setup required input
+    outDir = pathlib.Path(tmp_path)
+    dirPath = pathlib.Path(__file__).parents[0]
+    pathPlot = dirPath / 'data' / 'release1HX_ent_dfa_3f8d2d9327_pfd.png'
+    plotListRep = {'pfd': pathPlot}
+    avaName = 'avaTest'
+    testName = 'avaTestName'
+
+    # call function to be tested
+    plotPaths = gR.copyQuickPlots(avaName, testName, outDir, plotListRep, rel='')
+    testFile = outDir / 'avaTestName__pfd.png'
+
+    assert str(plotPaths['pfd']) == str(testFile)
+    assert len(plotPaths) == 1
+
+
+def test_copyAimecPlots(tmp_path):
+    """ test copying aimec plots to report location """
+
+    # setup required path
+    outDir = pathlib.Path(tmp_path)
+    dirPath = pathlib.Path(__file__).parents[0]
+    pathPlot = dirPath / 'data' / 'release1HX_ent_dfa_3f8d2d9327_pfd.png'
+    plotFiles = [{'aimec plot 1': pathPlot}, {'aimec plot 2': pathPlot}]
+    testName = 'avaTest'
+    plotPaths = {}
+
+    # call function to be tested
+    plotPaths = gR.copyAimecPlots(plotFiles, testName, outDir, plotPaths)
+    testFile = outDir / 'avaTest__release1HX_ent_dfa_3f8d2d9327_pfd.png'
+
+    assert str(plotPaths['aimec plot 1']) ==  str(testFile)
+    assert len(plotPaths) == 2
+
+
+def test_writeCompareReport(tmp_path):
+    """ test writing report file """
+
+    # setup required input
+    reportFile = pathlib.Path(tmp_path, 'reportTest.md')
+    avaName = 'avaTest'
+    cfg = configparser.ConfigParser()
+    cfg['GENERAL'] = {'diffLim': '0.01', 'perDiff': '0.01'}
+    reportD = {'avaName': {'type': 'avaName', 'name': 'data/avaKotST'},
+        'simName': {'type': 'simName', 'name': 'relKot_null_dfa_a50bf70f08'},
+        'time': {'type': 'time', 'time': '02/09/2021 13:20:17'},
+        'Simulation Parameters': {'type': 'list',
+                                  'Program version': 'development',
+                                  'Mu': '0.15500',
+                                  'Density [kgm-3]': '200'},
+        'Simulation Results': {'ppr': 'testplot.png',
+                               'Aimec comparison of mean and max values along path': 'testplot.png'},
+        'Aimec analysis': {'type': 'list', 'runout [m]': 16.,
+                           'max peak pressure [kPa]': 361.,
+                           'max peak flow depth [m]': 5.4,
+                           'max peak flow velocity [ms-1]': 42.50},
+        'Simulation Difference': {'ppr': [243., -2.2, -166.],
+                                  'pfd': [0.7, -0.01, -0.9],
+                                  'pfv': [34., -0.2, -28.]},
+        'Simulation Stats': {'ppr': [364., 0.0], 'pfd': [5., 0.0], 'pfv': [42., 0.0]}}
+    benchD = {'avaName': {'type': 'avaName', 'name': 'data/avaKotST'},
+        'simName': {'type': 'simName', 'name': 'relKot_null_dfa_0.15500'},
+        'time': {'type': 'time', 'time': '02/09/2021 13:19:50'},
+        'Simulation Parameters': {'type': 'list',
+                                  'Mu': '0.15500',
+                                  'Stop criterion': 'end time reached: 112.40 s',
+                                  'Avalanche run time [s]': 112.4},
+
+        'Release Area': {'type': 'columns', 'Release area scenario': 'relKot',
+                         'Release features': ['KoT'],
+                         'Release thickness [m]': [1.0]},
+        'Simulation Results': {'pfd': 'testplot.png',
+                               'type': 'image'},
+        'Aimec analysis': {'type': 'list', 'runout [m]': 16.,
+                           'max peak pressure [kPa]': 358.,
+                           'max peak flow depth [m]': 5.49,
+                           'max peak flow velocity [ms-1]': 42.},
+        'Test Info': {'type': 'text',
+                      'Test Info': 'Compare com1DFAOrig (Reference) to com1DFA (Simulation) results.'}}
+
+
+    # call function to be tested
+    gR.writeCompareReport(reportFile, reportD, benchD, avaName, cfg)
+
+
+    # Load simulation report
+    reportFile = open(os.path.join(tmp_path, 'reportTest.md'), 'r')
+    lines = reportFile.readlines()
+    lineVals = []
+    for line in lines:
+        lineVals.append(line)
+
+    reportFile.close()
+
+    assert lineVals[0] == '## *avaTest* \n'
+    assert lineVals[1] == '### Simulation name: *relKot_null_dfa_a50bf70f08* \n'
+    assert '#### <span style="color:red"> Reference simulation name is different' in lineVals[2]
+    assert 'relKot_null_dfa_0.15500  </span> \n' in lineVals[2]
+    assert lineVals[4] == '#### Test Info \n'
+    assert lineVals[6] == 'Compare com1DFAOrig (Reference) to com1DFA (Simulation) results. \n'
+    assert lineVals[8] == '#### Simulation Parameters \n'
+    assert lineVals[10] == '| Parameter | Reference | Simulation | \n'
+    assert lineVals[12] == '| Program version | non existent | <span style="color:red"> development </span> | \n'
+    assert lineVals[17] == '#### Aimec Analysis \n'
+    assert lineVals[20] == '| --------- | --------- | ---------- | \n'
+    assert lineVals[21] == '| runout [m] | 16.0 | 16.0 | \n'
+    assert lineVals[24] == '| max peak flow velocity [ms-1] | 42.0 | <span style="color:red"> 42.5 </span> | \n'
+    assert lineVals[27] == '#### Comparison Plots \n'
+    assert lineVals[29] == '##### Figure:   ppr \n'
+    assert lineVals[31] == ' <span style="color:red"> Warning absolute difference exceeds the tolerance of 1 percent of ppr-max value (364.00) </span> \n'
+    assert lineVals[32] == ' <span style="color:red"> Difference is: Max = 243.00, Mean = -2.20 and Min = -166.00 </span> \n'
+    assert lineVals[34] == '![ppr](testplot.png) \n'
+    assert lineVals[37] == '##### Figure:   Aimec comparison of mean and max values along path \n'
+    assert lineVals[39] == '![Aimec comparison of mean and max values along path](testplot.png) \n'
--- avaframe/tests/test_generateReport.py
+++ avaframe/tests/test_generateReport.py
@@ -12,6 +12,28 @@ from avaframe.log2Report import generateReport as gR
 
 import pytest
 import configparser
+import pathlib
+
+
+def test_addLineBlock(tmp_path):
+    """ test adding a line block """
+
+    # setup required input
+    reportDKey = {'type': 'class', 'name': 'TestName'}
+    testReport = pathlib.Path(tmp_path, 'testReport.md')
+    with open(testReport, 'w') as pfile:
+
+        # call function to be tested
+        gR.addLineBlock('##Hallo: ', reportDKey, pfile)
+        pfile.close()
+
+    reportFile = open(testReport, 'r')
+    lines = reportFile.readlines()
+    lineVals = []
+    for line in lines:
+        lineVals.append(line)
+
+    assert lineVals[0] == '##Hallo:  TestName \n'
 
 
 def test_writeReport(tmp_path):
@@ -22,22 +44,23 @@ def test_writeReport(tmp_path):
                 'testNo': 'this should not appear in the report if working fine!'},
         'avaName': {'type': 'avaName', 'name': 'data/avaTest'},
         'time': {'type': 'time', 'time': '1/01/2021 12:46:56'},
-        'simName': {'type': 'simName', 'name': 'this is my simulation name'},
+        'simName': {'type': 'simName', 'name': 'thisIsMySimulation1'},
         'Testing Parameters': {'release area': 'release1HS2', 'Mu': '0.155', 'type': 'list'},
         'Release area': {'release area scenario': 'my test release area', 'type': 'columns'},
         'Special Input': {'Additional snow-covered area': 'Area 1', 'type': 'columns'},
         'Additional Info': {'type': 'text',
                             'Report Info': 'This simulations were performed with a developement version.'},
         'Fancy plots': {'type': 'image',
-                        'Peak Pressure Field of my test' : 'release1HS2_entres_dfa_0.750_pfd.png'}}
+                        'Peak Pressure Field of my test': 'release1HS2_entres_dfa_0.750_pfd.png'}}
 
     # make a list of input dictionaries
     reportDictList = [testDict]
 
     # initialise cfg object and test directory
     cfg = configparser.ConfigParser()
-    cfg['FLAGS'] = {'reportOneFile': True}
-    plotDict = {'this is my simulation name': {'pfd': 'testPath/pfd.png'}}
+    cfg['FLAGS'] = {'reportOneFile': 'True'}
+    plotDict = {'thisIsMySimulation1': {'pfd': 'testPath/pfd.png'},
+                'simulationNumber2': {'pfd': 'testPath/pfd.png'}}
     # Call function to be tested
     gR.writeReport(tmp_path, reportDictList, cfg['FLAGS'], plotDict)
 
@@ -47,6 +70,8 @@ def test_writeReport(tmp_path):
     lineVals = []
     for line in lines:
         lineVals.append(line)
+
+    reportFile.close()
 
     # Test
     assert lineVals[0] == '# This is my report title \n'
@@ -58,3 +83,31 @@ def test_writeReport(tmp_path):
     assert lineVals[24] == '| ----------| \n'
     assert lineVals[-3] == '![pfd](testPath/pfd.png) \n'
     assert lineVals[37] == '![Peak Pressure Field of my test](release1HS2_entres_dfa_0.750_pfd.png) \n'
+
+    # call function to be tested
+    # make a list of input dictionaries
+    testDict2 = testDict
+    testDict2['simName']['name'] = 'simulationNumber2'
+    reportDictList = [testDict, testDict]
+    cfg['FLAGS'] = {'reportOneFile': 'False'}
+    gR.writeReport(tmp_path, reportDictList, cfg['FLAGS'], plotDict)
+
+    # Load simulation report
+    reportFile2 = open(os.path.join(tmp_path, 'simulationNumber2.md'), 'r')
+    lines = reportFile2.readlines()
+    lineVals2 = []
+    for line in lines:
+        lineVals2.append(line)
+
+    reportFile2.close()
+    print('lineVals2', lineVals2)
+
+    assert lineVals2[0] == '# This is my report title \n'
+    assert lineVals2[2] == '### Avalanche directory: *data/avaTest* \n'
+    assert lineVals2[4] == '### Date: 1/01/2021 12:46:56 \n'
+    assert lineVals2[11] == '| ---------- | ------ | \n'
+    assert lineVals2[12] == '| release area | release1HS2 | \n'
+    assert lineVals2[23] == '| Additional snow-covered area | \n'
+    assert lineVals2[24] == '| ----------| \n'
+    assert lineVals2[-3] == '![pfd](testPath/pfd.png) \n'
+    assert lineVals2[37] == '![Peak Pressure Field of my test](release1HS2_entres_dfa_0.750_pfd.png) \n'
