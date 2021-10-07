c588266c39d3dc1df66ce58da35093a60a698f92
matthiasto <matthias@avaframe.org>
2021-10-07 07:56:24 +0200

    rework plots

--- avaframe/runScripts/runSimilaritySol.py
+++ avaframe/runScripts/runSimilaritySol.py
@@ -17,6 +17,7 @@ import avaframe.in3Utils.fileHandlerUtils as fU
 from avaframe.in3Utils import cfgUtils
 from avaframe.in3Utils import logUtils
 import avaframe.ana1Tests.simiSolTest as simiSolTest
+import avaframe.com1DFA.com1DFA as com1DFA
 
 
 # +++++++++SETUP CONFIGURATION++++++++++++++++++++++++
@@ -28,7 +29,8 @@ cfgMain = cfgUtils.getGeneralConfig()
 avalancheDir = 'data/avaSimilaritySol'
 
 # Clean input directory(ies) of old work and output files
-initProj.cleanSingleAvaDir(avalancheDir, keep=logName)
+# initProj.cleanSingleAvaDir(avalancheDir, keep=logName)
+initProj.cleanModuleFiles(avalancheDir, module=com1DFA)
 
 # Start logging
 log = logUtils.initiateLogger(avalancheDir, logName)
