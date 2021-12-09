c578d87477be0665d045f985420ec5a040566815
matthiasto <matthias.tonnel@bfw.gv.at>
2021-12-09 10:42:46 +0100

    add sims

--- avaframe/runScripts/runAnalyseSimilaritySol.py
+++ avaframe/runScripts/runAnalyseSimilaritySol.py
@@ -62,22 +62,25 @@ solSimi = simiSolTest.mainSimilaritySol(simiSolCfg)
 # ascendingOrder = cfg['ANALYSIS']['ascendingOrder']
 # # load info for all configurations and order them
 # simDF = simDF.sort_values(by=varParList, ascending=ascendingOrder)
-simDF = simiSolTest.postProcessSimiSol(avalancheDir, cfgMain, cfg['SIMISOL'], simDF, solSimi, outDirTest)
 
 # if the analysis already exists and you only want to replot uncoment this
 pathToResults = pathlib.Path(avalancheDir, 'Outputs', 'ana1Tests', 'results.p')
-simDF = pd.read_pickle(pathToResults)
+if pathToResults.is_file():
+    simDF = pd.read_pickle(pathToResults)
+simDF = simiSolTest.postProcessSimiSol(avalancheDir, cfgMain, cfg['SIMISOL'], simDF, solSimi, outDirTest)
+
 
 # select the simulations you want to plot
-simDF = simDF[simDF['subgridMixingFactor'].isin([10])]
-simDF = simDF[simDF['viscOption'].isin([1])]
+# simDF = simDF[simDF['subgridMixingFactor'].isin([10])]
+# simDF = simDF[simDF['viscOption'].isin([1])]
 simDF = simDF[simDF['sphKernelRadius'].isin([10])]
+simDF = simDF[simDF['deltaTh'].isin([0.25, 0.1, 0.05])]#, 0.1, 0.05
 
 # now do some plotting
-# # compare the simulations to the reference
-# outAna1Plots.plotErrorRef(simDF, outDirTest, cfg['SIMISOL'], 'nPart', ['hErrorL2', 'vhErrorL2'],
-#                           'sphKernelRadius', 'dt', logScale=True)
+# compare the simulations to the reference
+outAna1Plots.plotErrorRef(simDF, outDirTest, cfg['SIMISOL'], 'subgridMixingFactor', ['hErrorL2', 'vhErrorL2'],
+                          'deltaTh', 'dt', logScale=False)
 
 # make convergence plot
-outAna1Plots.plotErrorConvergence(simDF, outDirTest, cfg['SIMISOL'], 'nPart', ['hErrorL2', 'vhErrorL2'],
-                          'sphKernelRadius', 'dt', logScale=True)
+# outAna1Plots.plotErrorConvergence(simDF, outDirTest, cfg['SIMISOL'], 'nPart', ['hErrorL2', 'vhErrorL2'],
+#                           'sphKernelRadius', 'dt', logScale=True)
