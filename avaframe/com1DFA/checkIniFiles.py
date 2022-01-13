4a3ba12babba143fff978d61f369ddb83e3c55ae
belieresfrendo <amaury.belieres---frendo@ensta-paris.fr>
2022-01-13 18:25:00 +0100

    Errors raising Last modification on checkIniFiles

--- avaframe/com1DFA/checkIniFiles.py
+++ avaframe/com1DFA/checkIniFiles.py
@@ -9,14 +9,12 @@ import sys
 log = logging.getLogger(__name__)
 
 def checkIniFiles(cfg):
+    # Check if Ata Parameters all well seted
     sphOption = float(cfg['GENERAL']['sphOption'])
     viscOption = float(cfg['GENERAL']['viscOption'])
-    log.info("sphOption :")
-    log.info(sphOption)
-    log.info("viscOption :")
-    log.info(viscOption)
     if viscOption == 2 and sphOption != 2:
         # raise an error
         message = 'If viscOption is set to 2 (Ata viscosity), you have to use sphOption to 2, or implement the Ata voscosity for the other sphOption values'
         log.error(message)
         raise AssertionError(message)
+    log.info('Ata Parameters are well seted')
