d12fcda813ed78999895dfa865147124ebe3a9ed
belieresfrendo <amaury.belieres---frendo@ensta-paris.fr>
2022-01-14 11:12:22 +0100

    making the presentation more compact -> putting every results in a dictionnary

--- avaframe/out3Plot/outInfluenceTest.py
+++ avaframe/out3Plot/outInfluenceTest.py
@@ -16,11 +16,11 @@ from avaframe.in3Utils import fileHandlerUtils as fU
 # change log level in calling module to DEBUG to see log messages
 log = logging.getLogger(__name__)
 
-def analyzeResults(simDF, avalancheDir, IDList, refID, parameter2Study, visc2Study, otherParamValueList, otherParamNameList, xAxisArray):
+def analyzeResults(simDF, avalancheDir, IDList, refID, resInfluenceTest):
     """ generate Lists to plot results
     Parameters
     ----------
-    simDF, avalancheDir, samosIDList, ataIDList, refRunout, parameter2Study, visc2Study, otherParamValueList, otherParamNameList
+    simDF, avalancheDir, samosIDList, ataIDList, refRunout, param2Study, visc2Study, otherParamValueList, otherParamNameList
         all described in previous functions
     Returns
     -------
@@ -31,6 +31,11 @@ def analyzeResults(simDF, avalancheDir, IDList, refID, parameter2Study, visc2Stu
     MMPFDArray: float List
         list of every graph MMPFD
     """
+
+    otherParamNameList = resInfluenceTest['otherParamNameList']
+    otherParamValueList = resInfluenceTest['otherParamValueList']
+    param2Study = resInfluenceTest['param2Study']
+    xAxisArray = resInfluenceTest['xAxisArray']
 
     runoutArray = []
     MMPPRArray = []
@@ -45,7 +50,7 @@ def analyzeResults(simDF, avalancheDir, IDList, refID, parameter2Study, visc2Stu
             MMPFDArray.append([])
             for xCoord in xAxisArray:
                 for id in IDList:
-                    if simDF.loc[id][paramN] == float(paramV) and simDF.loc[id][parameter2Study] == xCoord:
+                    if simDF.loc[id][paramN] == float(paramV) and simDF.loc[id][param2Study] == xCoord:
                         runoutArray[iV].append(simDF.loc[id]['runout'])
                         MMPPRArray[iV].append(simDF.loc[id]['MMPPR'])
                         MMPFDArray[iV].append(simDF.loc[id]['MMPFD'])
@@ -56,21 +61,21 @@ def analyzeResults(simDF, avalancheDir, IDList, refID, parameter2Study, visc2Stu
         refArray[1].append(simDF.loc[refID]['MMPPR'])
         refArray[2].append(simDF.loc[refID]['MMPFD'])
 
-    print('runoutArray : ', runoutArray)
-    print('MMPPRArray : ', MMPPRArray)
-    print('MMPFDArray : ', MMPFDArray)
-    print('refArray : ', refArray)
-    print('len(runoutArray) : ', len(runoutArray))
 
-    return runoutArray, MMPPRArray, MMPFDArray, refArray
+    resInfluenceTest['runoutArray'] = runoutArray
+    resInfluenceTest['MMPPRArray'] = MMPPRArray
+    resInfluenceTest['MMPFDArray'] = MMPFDArray
+    resInfluenceTest['refArray'] = refArray
+
+    return resInfluenceTest
 
 
 
-def plotResults(avalancheDir, parameter2Study, visc2Study, otherParamNameList, otherParamValueList, xAxisArray, runoutArray, MMPPRArray, MMPFDArray, refArray):
+def plotResults(avalancheDir, resInfluenceTest):
     """ generate Plots contained in xAxisArray, runoutArray, MMPPRArray, MMPFDArray and refArray lists
     Parameters
     ----------
-    parameter2Study, visc2Study, otherParamNameList, otherParamValueList, xAxisArray, runoutArray, MMPPRArray, MMPFDArray
+    param2Study, visc2Study, otherParamNameList, otherParamValueList, xAxisArray, runoutArray, MMPPRArray, MMPFDArray
         all described in previous functions
     """
 
@@ -79,9 +84,19 @@ def plotResults(avalancheDir, parameter2Study, visc2Study, otherParamNameList, o
     log.info(outDir)
     fU.makeADir(outDir)
 
+    otherParamNameList = resInfluenceTest['otherParamNameList']
+    otherParamValueList = resInfluenceTest['otherParamValueList']
+    param2Study = resInfluenceTest['param2Study']
+    visc2Study = resInfluenceTest['visc2Study']
+    xAxisArray = resInfluenceTest['xAxisArray']
+    runoutArray = resInfluenceTest['runoutArray']
+    MMPPRArray = resInfluenceTest['MMPPRArray']
+    MMPFDArray = resInfluenceTest['MMPFDArray']
+    refArray = resInfluenceTest['refArray']
+
     for iN in range(len(otherParamNameList)):
         paramN = otherParamNameList[iN]
-        plt.xlabel(parameter2Study)
+        plt.xlabel(param2Study)
 
         plt.ylabel('runout')
         for iV in range(len(otherParamValueList[iN])):
@@ -89,25 +104,25 @@ def plotResults(avalancheDir, parameter2Study, visc2Study, otherParamNameList, o
             plt.plot(xAxisArray,  runoutArray[iV], label=visc2Study + ' viscosity, ' + paramN + ' = ' + paramV)
         plt.plot(xAxisArray,  refArray[0], label='ref')
         plt.legend()
-        plt.savefig(outDir + '/Runout_' + visc2Study + 'Viscosity_param2Study_' + parameter2Study + '_' + paramN + '_' + paramV + '.png')
+        plt.savefig(outDir + '/Runout_' + visc2Study + 'Viscosity_param2Study_' + param2Study + '_' + paramN + '_' + paramV + '.png')
         plt.show()
 
-        plt.xlabel(parameter2Study)
+        plt.xlabel(param2Study)
         plt.ylabel('MMPPR')
         for iV in range(len(otherParamValueList[iN])):
             paramV = otherParamValueList[iN][iV]
             plt.plot(xAxisArray,  MMPPRArray[iV], label=visc2Study + ' viscosity, ' + paramN + ' = ' + paramV)
         plt.plot(xAxisArray,  refArray[1], label='ref')
         plt.legend()
-        plt.savefig(outDir + '/MMPPR' + visc2Study + 'Viscosity_param2Study_' + parameter2Study + '_' + paramN + '_' + paramV + '.png')
+        plt.savefig(outDir + '/MMPPR' + visc2Study + 'Viscosity_param2Study_' + param2Study + '_' + paramN + '_' + paramV + '.png')
         plt.show()
 
-        plt.xlabel(parameter2Study)
+        plt.xlabel(param2Study)
         plt.ylabel('MMPFD')
         for iV in range(len(otherParamValueList[iN])):
             paramV = otherParamValueList[iN][iV]
             plt.plot(xAxisArray,  MMPFDArray[iV], label=visc2Study + ' viscosity, ' + paramN + ' = ' + paramV)
         plt.plot(xAxisArray,  refArray[2], label='ref')
         plt.legend()
-        plt.savefig(outDir + '/MMPFD' + visc2Study + 'Viscosity_param2Study_' + parameter2Study + '_' + paramN + '_' + paramV + '.png')
+        plt.savefig(outDir + '/MMPFD' + visc2Study + 'Viscosity_param2Study_' + param2Study + '_' + paramN + '_' + paramV + '.png')
         plt.show()
