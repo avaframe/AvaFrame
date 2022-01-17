b6c2abb69fba09a29615820dfce9d3c7b5690a31
matthiasto <matthias.tonnel@bfw.gv.at>
2022-01-17 13:21:05 +0100

    fix hybrid aimec

--- avaframe/com3Hybrid/com3Hybrid.py
+++ avaframe/com3Hybrid/com3Hybrid.py
@@ -102,14 +102,14 @@ def maincom3Hybrid(cfgMain, cfgHybrid):
         iterate = keepIterating(cfgHybrid, alphaArray)
         iteration = iteration + 1
 
-    pathDict, rasterTransfo, resAnalysis = runAna3AIMEC.runAna3AIMEC(avalancheDir=avalancheDir)
+    pathDict, rasterTransfo, resAnalysisDF = runAna3AIMEC.runAna3AIMEC(avalancheDir=avalancheDir)
     simID = simDF.index[0]
-    indSim = pathDict['simID'].index(simID)
+    refSimulation = pathDict['refSimulation']
     # fetch fields for desired time step
     timeSteps = [timeStepInfo[-1]]
     fields = com1DFATools.fetchField(timeSteps, 'pta', avalancheDir, inputDir='')
-    outCom3Plots.finalizePathPlot(avalancheDir, figDict, resAnalysis, indSim, dem, demOri, particlesList[-1], fields[0])
-    outCom3Plots.finalizeProfilePlot(avalancheDir, figDict, resAnalysis, indSim)
+    outCom3Plots.finalizePathPlot(avalancheDir, figDict, resAnalysisDF, refSimulation, dem, demOri, particlesList[-1], fields[0])
+    outCom3Plots.finalizeProfilePlot(avalancheDir, figDict, resAnalysisDF, refSimulation)
 
     hybridModelDFACfg = pathlib.Path('com3Hybrid', 'hybridModel_com1DFACfg.ini')
     cfgDFA = cfgUtils.getModuleConfig(com1DFA, fileOverride=hybridModelDFACfg)
