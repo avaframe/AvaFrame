e2fa1eee0b22a1ce894b68edcf941d89ce01a8f8
matthiasto <matthias.tonnel@bfw.gv.at>
2022-02-21 15:08:10 +0100

    add doc

--- avaframe/ana1Tests/ana1EnergyLineTest.py
+++ avaframe/ana1Tests/ana1EnergyLineTest.py
@@ -8,6 +8,7 @@ import pathlib
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
 from scipy import interpolate
 
 # Local imports
@@ -76,19 +77,24 @@ def mainEnergyLineTest(cfgMain):
 
     g = energyLineTestCfg['GENERAL'].getfloat('gravAcc')
     mu = energyLineTestCfg['GENERAL'].getfloat('mu')
-    alpha = np.rad2deg(np.arctan(mu))
+    alphaRad = np.arctan(mu)
+    alphaDeg = np.rad2deg(alphaRad)
     deltaS = avaProfileMass['s'][-1] - avaProfileMass['s'][0]
     deltaZ = avaProfileMass['z'][-1] - avaProfileMass['z'][0]
-    runOutAngle = np.rad2deg(np.arctan(np.abs(deltaZ/deltaS)))
+    runOutAngleRad = np.arctan(np.abs(deltaZ/deltaS))
+    runOutAngleDeg = np.rad2deg(runOutAngleRad)
     p1 = np.polyfit(avaProfileMass['s'][-3:], avaProfileMass['z'][-3:], 1)
 
     cmap, _, ticks, norm = pU.makeColorMap(pU.colorMaps['pfv'], np.amin(V2Path/(2*g)), np.amax(V2Path/(2*g)),
                                            continuous=pU.contCmap)
     cmap.set_under(color='w')
-    Zene = avaProfileMass['z'] + V2Path/(2*g)
 
+    # compute mass average velocity elevation
+    zEne = avaProfileMass['z'] + V2Path/(2*g)
+    # Create the alpha line
     GK = 1.05*avaProfileMass['s'][-1] * mu
     z_end = avaProfileMass['z'][0] - GK
+    zEneTarget = avaProfileMass['z'][0] - avaProfileMass['s'] * mu
     s_geomL = [avaProfileMass['s'][0], 1.05*avaProfileMass['s'][-1]]
     z_geomL = [avaProfileMass['z'][0], z_end]
 
@@ -105,26 +111,35 @@ def mainEnergyLineTest(cfgMain):
     zA0 = alphaLine[idx]
     zA1 = alphaLine[idx+1]
     sIntersection = s0 + (s1-s0)*(zA0-zP0)/((zP1-zP0)-(zA1-zA0))
+    zIntersection = zP0 + (sIntersection-s0) * (zP1-zP0) / (s1-s0)
+
+    # compute errors
+    # rmse of the energy height
+    rmseVelocityElevation = rmse(zEne, zEneTarget)
+    # error on s runout
+    runOutSError = avaProfileMass['s'][-1] - sIntersection
+    # error on z runout
+    runOutZError = avaProfileMass['z'][-1] - zIntersection
+    # error on angle runout
+    runOutAngleError = runOutAngleDeg - alphaDeg
 
     ax2 = plt.subplot2grid((2, 2), (0, 0), colspan=2)
     # plot mass averaged center of mass
     ax2.plot(avaProfileMass['s'], avaProfileMass['z'], '-k.', label='Center of mass altitude')
-    # extend this curve towards the bottom using a linear gegression on the last x points
+    # ax2.plot(avaProfileMass['sCor'], avaProfileMass['z'], '--k.', label='Center of mass altitude (corrected s)')
+    # extend this curve towards the bottom using a linear regression on the last x points
     ax2.plot(avaProfileMass['s'][-1]*np.array([1, 1.05]), avaProfileMass['z'][-1] + p1[0]*avaProfileMass['s'][-1]*np.array([0, 0.05]),
              ':k', label='Center of mass altitude extrapolation')
 
     # add center of mass velocity points and run-out line
-    ax2.plot(avaProfileMass['s'][[0, -1]], Zene[[0, -1]], '-r', label='run-out line %.2f °' % runOutAngle)
-    scat = ax2.scatter(avaProfileMass['s'], Zene, marker='o', cmap=cmap, s=2*pU.ms, c=V2Path/(2*g),
+    ax2.plot(avaProfileMass['s'][[0, -1]], zEne[[0, -1]], '-r', label='com1dfa energy line (%.2f°)' % runOutAngleDeg)
+    scat = ax2.scatter(avaProfileMass['s'], zEne, marker='o', cmap=cmap, s=2*pU.ms, c=V2Path/(2*g),
                        label='Center of mass velocity altitude')
     cbar2 = ax2.figure.colorbar(scat, ax=ax2, use_gridspec=True)
     cbar2.ax.set_ylabel('Center of mass velocity altitude [m]')
 
     # add alpha line
-    ax2.plot(s_geomL, z_geomL, 'b-', label=r'$\alpha$ line %.2f °' % alpha)
-
-    # add horizontal line at the final mass averaged position
-    ax2.axhline(y=avaProfileMass['z'][-1], color='k', linestyle='--', label='horizontal line at z final')
+    ax2.plot(s_geomL, z_geomL, 'b-', label=r'$\alpha$ line (%.2f°)' % alphaDeg)
 
     ax2.set_xlabel('s [m]')
     ax2.set_ylabel('z [m]')
@@ -140,27 +155,37 @@ def mainEnergyLineTest(cfgMain):
              ':k', label='Center of mass altitude extrapolation')
 
     # add center of mass velocity points and run-out line
-    ax3.plot(avaProfileMass['s'][[0, -1]], Zene[[0, -1]], '-r', label='run-out line %.2f °' % runOutAngle)
-    scat = ax3.scatter(avaProfileMass['s'], Zene, marker='o', cmap=cmap, s=2*pU.ms, c=V2Path/(2*g),
+    ax3.plot(avaProfileMass['s'][[0, -1]], zEne[[0, -1]], '-r', label='com1dfa energy line (%.2f°)' % runOutAngleDeg)
+    scat = ax3.scatter(avaProfileMass['s'], zEne, marker='o', cmap=cmap, s=2*pU.ms, c=V2Path/(2*g),
                        label='Center of mass velocity altitude extrapolation')
     cbar3 = ax3.figure.colorbar(scat, ax=ax3, use_gridspec=True)
     cbar3.ax.set_ylabel('Center of mass velocity altitude [m]')
 
     # add alpha line
-    ax3.plot(s_geomL, z_geomL, 'b-', label=r'$\alpha$ line %.2f °' % alpha)
+    ax3.plot(s_geomL, z_geomL, 'b-', label=r'$\alpha$ line (%.2f°)' % alphaDeg)
 
     # add horizontal line at the final mass averaged position
-    ax3.axhline(y=avaProfileMass['z'][-1], color='k', linestyle='--')
-    ax3.axvline(x=avaProfileMass['s'][-1], color='r', linestyle='--')
-    ax3.axvline(x=sIntersection, color='b', linestyle='--')
+    errorS = abs(sIntersection - avaProfileMass['s'][-1])
+    ax3.vlines(x=avaProfileMass['s'][-1], ymin=avaProfileMass['z'][-1]-mu*2*errorS, ymax=avaProfileMass['z'][-1], color='r', linestyle='--')
+    ax3.vlines(x=sIntersection, color='b', ymin=avaProfileMass['z'][-1]-mu*2*errorS, ymax=zIntersection, linestyle='--')
 
     ax3.set_xlabel('s [m]')
     ax3.set_ylabel('z [m]')
     # ax3.legend(loc='upper right')
-    errorS = abs(sIntersection - avaProfileMass['s'][-1])
     ax3.set_ylim([avaProfileMass['z'][-1]-mu*2*errorS, avaProfileMass['z'][-1]+mu*2*errorS])
     ax3.set_xlim([avaProfileMass['s'][-1]-2*errorS, avaProfileMass['s'][-1]+2*errorS])
     ax3.get_yaxis().set_ticks([])
     ax3.tick_params(axis='x', which='major', labelsize=8, rotation=45)
     ax3.set_xticks([avaProfileMass['s'][-1], sIntersection])
+    text = ('Run-out s diff : %.2f m \nRun-out z diff : %.2f m \nRun-out angle diff : %.4f° \nvelocity height rsme : %.2f m \n(energy line - alpha line)' %
+            (runOutSError, runOutZError, runOutAngleError, rmseVelocityElevation))
+    text_box = AnchoredText(text, frameon=False, loc=1, pad=0.5)
+    plt.setp(text_box.patch, facecolor='white', alpha=0.5)
+    ax3.add_artist(text_box)
+
     plt.show()
+
+
+def rmse(predictions, targets):
+    """ Compute the root mean square error between two numpy 1D arrays"""
+    return np.sqrt(((predictions - targets) ** 2).mean())
