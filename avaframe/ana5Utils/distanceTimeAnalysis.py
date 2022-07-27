Makes velocity legend in rangeTimePlots adopt to zoom/pan events of figure   - Removes explicit set of matplotlib backend 'agg'   - Fixes max front velocity returns sometimes obviously wrong values (e.g. maxV of 400m/s inside release area at first timestep)   - add option to use the max or the average value of the fields in each cross section   - Addresse variable naming; fix #745 #573   - remove appraoch vel other appraoch, remove max vel from animation plot   - udpate pytest for approach vel

avaframe/ana5Utils/distanceTimeAnalysis.py
@@ -6,21 +6,15 @@
 
 
 import numpy as np
-import matplotlib as mpl
-from matplotlib import pyplot as plt
 import logging
 import pickle
 import pathlib
-import configparser
 
 # Local imports
 from avaframe.in3Utils import cfgUtils
-from avaframe.in3Utils import logUtils
-import avaframe.in2Trans.ascUtils as IOf
 import avaframe.in3Utils.geoTrans as gT
 import avaframe.out3Plot.outDistanceTimeAnalysis as dtAnaPlots
 import avaframe.ana3AIMEC.aimecTools as aT
-import avaframe.out3Plot.plotUtils as pU
 import avaframe.com1DFA.com1DFA as com1DFA
 import avaframe.in3Utils.fileHandlerUtils as fU
 
@@ -238,7 +232,7 @@ def extractFrontAndMeanValuesRadar(cfgRangeTime, flowF, mtiInfo):
     # mask range with radar field of view and treshold of flow variable result
     maskAva, bmaskAvaRadar, rMaskedAvaRadar = maskRangeFull(flowF, threshold, rangeMasked)
 
-    #++++++++Eextract front location with respect to radar ++++++++++++++
+    # ++++++++Extract front location with respect to radar ++++++++++++++
     # get line of sight distance to identify front, use threshold to mask flowF
     # line of sight min of masked array
     losDistance = rMaskedAvaRadar.min()
@@ -246,7 +240,7 @@ def extractFrontAndMeanValuesRadar(cfgRangeTime, flowF, mtiInfo):
     # update lists of time step and front location
     mtiInfo['rangeList'].append(losDistance)
 
-    #+++++++Extract average values at range gates +++++++++
+    # +++++++Extract average values at range gates +++++++++
     # min and max radar range of masked radar range
     if not rMaskedAvaRadar.mask.all():
         #TODO why int?
@@ -438,7 +432,6 @@ def extractFrontAndMeanValuesTT(cfgRangeTime, flowF, demHeader, mtiInfo):
     # TODO: average over cells – weighted with cell area (aimec function)
     rasterArea = rasterTransfo['rasterArea']
     maxaCrossMax, aCrossMax, aCrossMean = aT.getMaxMeanValues(slRaster, rasterArea)
-
     # use the max or the mean of each cross section
     if cfgRangeTime['GENERAL']['maxOrMean'].lower() == 'max':
         aCross = aCrossMax
@@ -699,22 +692,29 @@ def fetchTimeStepFromName(pathNames):
     return timeSteps, indexTime
 
 
-def approachVelocity(mtiInfo, minVelTimeStep):
-    """ compute approach velocity based on front location and time step
-        performed for a min time difference of minVelTimeStep - max velocity location is at the
-        center index location
+def approachVelocity(mtiInfo):
+    """ compute maximal approach velocity based on front location (range) and time step
+
+        - cleans nan in range
+        - neglects anything behind maximal runout
+        - neglects non-unique range values, e.g. the front did not move
+        - cleans approach velocity: velocity in cell under test can not be higher
+            than the double of the mean from the surrounding cells
+
 
         Parameters
         -----------
         mtiInfo: dict
             info on distance to front and time steps
-        minVelTimeStep: float
-            min time step difference for computing approach velocity
 
         Returns
         --------
         maxVel: float
             max value of approach velocity
+        rangeVel: float
+            range of max value of approach velocity
+        timeVel: float
+            time of max value of approach velocity
     """
 
     # load lists
@@ -731,16 +731,42 @@ def approachVelocity(mtiInfo, minVelTimeStep):
     rangeVel = 0.0
     timeVel = 0.0
 
-    # use minimum time step for computing approach velocity
-    for i in range(len(timeListSorted)):
-        for k in range(i+1, len(timeListSorted+1)):
-            if abs(timeListSorted[i] - timeListSorted[k]) >= minVelTimeStep:
-                appVel = ((rangeListSorted[i] - rangeListSorted[k]) / (timeListSorted[i] - timeListSorted[k]))
-                if appVel > maxVel:
-                    maxVel = appVel
-                    locationIndex = int(i + 0.5*(k-i))
-                    rangeVel = rangeListSorted[locationIndex] - abs(np.nanmin(np.asarray(rangeList)))
-                    timeVel = timeListSorted[locationIndex]
-                break
+    # remove nans in range
+    nanIdx = np.argwhere(~np.isnan(rangeListSorted)).squeeze()
+    # remove anything behind runout
+    rmaxIdx = np.arange(0, np.nanargmax(rangeListSorted)+1)
+    idx = np.intersect1d(nanIdx, rmaxIdx)
+
+    rangeListSortedSmall = rangeListSorted[idx]
+    timeListSortedSmall = timeListSorted[idx]
+    # remove non-unique range values
+    rangeListSortedUnique, uniqueIdx = np.unique(rangeListSortedSmall.round(decimals=2), return_index=True)
+    timeListSortedUnique = timeListSortedSmall[uniqueIdx]
+
+    # calculate approach velocity
+    appVel = np.diff(rangeListSortedUnique)/np.diff(timeListSortedUnique)
+    rangeAppVel = rangeListSortedUnique[0:-1]
+    timeAppVel = timeListSortedUnique[0:-1]
+
+    # clean approach velocity: Idea is that the velocity in one cell can not be bigger
+    # than the double of the mean in the surrounding cells
+    idxAppVel = []
+    for i in range(len(appVel)-1):
+        if i == 0:  # first cell
+            vSurrounding = appVel[1]
+        elif i == len(appVel)-1:  # last cell
+            vSurrounding = appVel[i-1]
+        else:  # other cells, take mean
+            vSurrounding = (appVel[i-1] + appVel[i+1]) / 2
+        if appVel[i] <= 2*vSurrounding:
+            idxAppVel.append(i)
+    appVelClean = appVel[idxAppVel]
+    rangeAppVelClean = rangeAppVel[idxAppVel]
+    timeAppVelClean = timeAppVel[idxAppVel]
+
+    idxMaxVel = np.argmax(appVelClean)
+    maxVel = appVelClean[idxMaxVel]
+    rangeVel = rangeAppVelClean[idxMaxVel] - abs(np.nanmin(np.asarray(rangeList)))
+    timeVel = timeAppVelClean[idxMaxVel]
 
     return maxVel, rangeVel, timeVel

avaframe/out3Plot/outDistanceTimeAnalysis.py
@@ -32,12 +32,12 @@ def plotRangeTime(mtiInfo, cfgRangeTime):
     """
 
     # fetch required input info
-    mti =  mtiInfo['mti']
+    mti = mtiInfo['mti']
     rangeGates = mtiInfo['rangeGates']
     timeList = mtiInfo['timeList']
     rangeList = mtiInfo['rangeList']
     rangeTimeResType = cfgRangeTime['GENERAL']['rangeTimeResType']
-    maxVel, rangeVel, timeVel = dtAna.approachVelocity(mtiInfo, cfgRangeTime['GENERAL'].getfloat('minVelTimeStep'))
+    maxVel, rangeVel, timeVel = dtAna.approachVelocity(mtiInfo)
 
     # in case time steps are not ordered - the colormesh x and y need to be ordered
     timeIndex = np.argsort(np.array(timeList))
@@ -55,8 +55,8 @@ def plotRangeTime(mtiInfo, cfgRangeTime):
     ax = fig.add_subplot(1, 1, 1)
     plt.title(mtiInfo['plotTitle'])
     pc = plt.pcolormesh(timeListNew, rangeGates, mti, cmap=pU.cmapRangeTime)
-    plt.plot(timeList, rangeList , '.', color='black', markersize=4,
-        label='avalanche front')
+    plt.plot(timeList, rangeList, '.', color='black', markersize=4,
+             label='avalanche front')
     plt.xlabel('Time [s]')
     # add y label axis
     if mtiInfo['type'] == 'thalwegTime':
@@ -67,17 +67,20 @@ def plotRangeTime(mtiInfo, cfgRangeTime):
 
     # add colorbar and infobox
     unit = pU.cfgPlotUtils['unit' + rangeTimeResType]
-    if mtiInfo['type'] == 'thalwegTime' and cfgRangeTime['GENERAL']['maxOrMean'] == 'max':
+    if mtiInfo['type'] == 'thalwegTime' and cfgRangeTime['GENERAL']['maxOrMean'].lower() == 'max':
         avgType = 'max'
     else:
         avgType = 'avg.'
     cName = '%s ' % avgType + pU.cfgPlotUtils['name' + rangeTimeResType]
     pU.addColorBar(pc, ax, None, unit, title=cName)
     pU.putAvaNameOnPlot(ax, cfgRangeTime['GENERAL']['avalancheDir'])
-    rangeTimeVelocityLegend(timeListNew, rangeGates, ax, maxVel, width=width, height=height, lw=lw, textsize=textsize)
+    # add range time velocity legend
+    rangeTimeVelocityLegend(ax, maxVel, width, height, lw, textsize)
+
     # add max velocity location
     ax.plot(timeVel, rangeVel, 'r*', label='max velocity location')
-    cbar.ax.axhline(y=maxVel, color='r', lw=1, label='max velocity')
+    #FSO: is this needed?
+    # cbar.ax.axhline(y=maxVel, color='r', lw=1, label='max velocity')
 
     # add info on avalanche front in legend
     plt.legend(facecolor='grey', framealpha=0.2, loc='lower right', fontsize=8)
@@ -87,7 +90,7 @@ def plotRangeTime(mtiInfo, cfgRangeTime):
         # invert y axis as ava flow starts from minus distance to beta point
         ax.invert_yaxis()
         ax.axhline(y=0.0, color='gray', linestyle='--', linewidth=1, alpha=0.5,
-            label='beta point: %.1f°' % mtiInfo['betaPointAngle'])
+                   label='beta point: %.1f°' % mtiInfo['betaPointAngle'])
 
     # set path for saving figure
     outDir = pathlib.Path(cfgRangeTime['GENERAL']['avalancheDir'], 'Outputs', 'ana5Utils')
@@ -304,83 +307,161 @@ def plotMaskForMTI(cfgRangeTime, bmaskRange, bmaskAvaRadar, bmaskAvaRadarRangesl
     pU.saveAndOrPlot(pathDict, outFileName, fig)
 
 
-def rangeTimeVelocityLegend(timeSteps, rangeGates, ax, maxVel, width=0.25, height=0.25, lw=0.5, textsize=7):
-    """ set legend in range time diagram for velocity in terms of steepness of front position
+def getMaxVelocityPoint(width, height, maxVel, diagVel):
+    """ get point of max approach velocity in axes coordinates
 
         Parameters
         -----------
-        timeSteps: list
-            time steps - needs to be ordered
-        rangeGates: list
-            list of range gates - needs to be ordered
-        ax: matplotlib axis object
-            axes where the legend shall be accomodated to
         width: float
-            width of legend in fraction of full plot width
+            fractional percentage of legend width
         height: float
-            height of legend in fraction of full plot height
-        lw: float
-            line width
-        textsize: float
-            size of text
+            fractional percentage of legend height
+        maxVel: float
+            maximum approach velocity value
+        diagVel: float
+            x/y ratio of axes
 
+        Returns
+        --------
+        point: tuple
+            x, y axes coordinates of maximum velocity
     """
 
-    # compute slope of max velocity line for transformed axes
-    xMax = timeSteps[-1]
-    yMax = np.amax(rangeGates)# - np.amin(rangeGates)
-    xinterval = xMax - timeSteps[0]
-    yinterval = np.amax(rangeGates) - np.amin(rangeGates)
-    kPlot = (yinterval /xinterval)
+    # along bottom line
+    if maxVel > diagVel:
+        point = [(1 - width) + width / maxVel * diagVel, 1 - height]
+    else:
+        # along re vert line
+        point = [1, 1 - height * maxVel / diagVel]
 
-    # fetch points for drawing velocity legend
-    point = getVelocityPoints(1, 1, width, height, 1, 1, maxVel, kPlot)
+    return point
+
+
+def rangeTimeVelocityLegend(ax, maxVel, width, height, lw, textsize):
+    """ set legend in range time diagram for velocity in terms of steepness of front position
+        connects to figure callback to react to zoom/pan events
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+        figure axes object
+    maxVel: float
+        maximum approach velocity value
+    width : float , *0.25
+        fractional percentage of legend width
+    height: float
+        fractional percentage of legend height
+    lw: float
+        linewidth for legend line
+    textsize: float
+        textsize for legend entries
+    """
+
+    # keep track of which text elements belong to this legend thing...
+    inititalNrOfTextElements = len(ax.texts)
+
+    # get legend points in axes coordinates
+    point = getVelocityPoints(1, 1, width, height, 1, 1)
 
     # add title and boundary box and diagonal lines using point
-    addTitleBox(ax, lw, point, maxVel=True)
+    lVmax = addTitleBox(ax, width, height, lw, point, textsize, maxVel=True)
 
-    # compute velocity values
-    dataPoint = getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval, maxVel, kPlot)
-    # add velocity legend labels
-    addVelocityValues(ax, dataPoint, point, maxVel=maxVel)
+    # ---- internal functions ----
+    def zoomLvlChange(eventAx, first=False):
+        """
+        Function to annotate the diagonal lines with velocity values.
+        Flag first decides if the text elements are created (must be called once)
+        or if just the text is changed (figure callback)
+        """
+
+        # get current axes limits
+        xdMax = max(eventAx.get_xlim())  # timesteps[-1]
+        ydMax = max(eventAx.get_ylim())  # max(rangegates)
+        xdIntervall = max(eventAx.get_xlim()) - min(eventAx.get_xlim())
+        ydIntervall = max(eventAx.get_ylim()) - min(eventAx.get_ylim())  # y_max-rangegates[0] # from min to max
+        vDiag = ( ydIntervall / xdIntervall)
+
+        # calculate current velocities of diagonal lines
+        dataPoint = getVelocityPoints(xdMax, ydMax, width, height, xdIntervall, ydIntervall)
+        textElementNr = addVelocityValues(ax, dataPoint, point, first=first, inititalNrOfTextElements=inititalNrOfTextElements)
+
+        # make or update max velocity line and text
+        maxVelPoint = getMaxVelocityPoint(width, height, maxVel, vDiag)
+        if maxVel < vDiag:
+            xLoc = maxVelPoint[0] - 0.04
+            yLoc = maxVelPoint[1] - 0.004
+        else:
+            xLoc = maxVelPoint[0]
+            yLoc = maxVelPoint[1] + 0.004
+        if first:
+                eventAx.text(xLoc, yLoc, 'max %.1f' % maxVel, size=5, fontweight='bold',
+                              color='r', transform=ax.transAxes, gid=len(point)-2, va='center', ha='left',
+                              bbox=dict(facecolor='white', alpha=0.5))
+        else:
+                ax.texts[textElementNr+1].set_position((xLoc, yLoc))
+                lVmax.set_data([point[0][0], maxVelPoint[0]], [point[0][1], maxVelPoint[1]])
+    # ---- internal functions end ----
+
+    # make the text, e.g. the mandatory first=True call
+    zoomLvlChange(ax, first=True)
+
+    # connect callback
+    ax.callbacks.connect('xlim_changed', zoomLvlChange)
+    ax.callbacks.connect('ylim_changed', zoomLvlChange)
 
 
-def addTitleBox(ax, lw, point, maxVel=True):
+def addTitleBox(ax, width, height, lw, point, textsize, maxVel=True):
     """ add velocity legend title and boundary box and diagonal lines
-        if maxVel=True add max velocity line
+        if maxVel=True add max velocity line in red
 
         Parameters
         -----------
-        point: list
-            list of points coordinates for velocity legend
+        ax: matplotlib axes object
+            axes object for legend
+        width: float
+            fractional percentage of legend width
+        height: float
+            fractional percentage of legend height
         lw: float
             linewidth
-        ax: matplotlib axis object
+        point: list
+            list of points coordinates for velocity legend
+        textsize: float
+            size of legend entries
         maxVel: bool
             if True add max velocity line in red
     """
 
-    # add title
-    ax.text(point[1][0], point[1][1], 'Approach \n velocity [m/s]', size=6,
+    # add legend title
+    ax.text(point[1][0], point[1][1], 'Approach \n velocity [m/s]', size=textsize,
         transform=ax.transAxes, va='top', ha='right', fontweight='bold',
         bbox=dict(facecolor='white', alpha=0.5))
 
     # plot boundary of velocity box
-    ax.plot([point[0][0], point[1][0]], [point[0][1], point[1][1]], color='k', linewidth=lw, transform=ax.transAxes)
-    ax.plot([point[1][0], point[5][0]], [point[1][1], point[5][1]], color='k', linewidth=lw, transform=ax.transAxes)
-    ax.plot([point[5][0], point[10][0]], [point[5][1], point[10][1]], color='k', linewidth=lw, transform=ax.transAxes)
-    ax.plot([point[10][0], point[0][0]], [point[10][1], point[0][1]], color='k', linewidth=lw, transform=ax.transAxes)
+    ax.plot([point[0][0], point[1][0]], [point[0][1], point[1][1]], color='k', linewidth=lw, transform=ax.transAxes) # top hor
+    ax.plot([point[1][0], point[5][0]], [point[1][1], point[5][1]], color='k', linewidth=lw, transform=ax.transAxes) # re vert
+    ax.plot([point[5][0], point[9][0]], [point[5][1], point[9][1]], color='k', linewidth=lw, transform=ax.transAxes) # bottom hor
+    ax.plot([point[9][0], point[0][0]], [point[9][1], point[0][1]], color='k', linewidth=lw, transform=ax.transAxes) # le vert
 
     # these are the diagonal lines referring to velocity
     for j in range(2, len(point) - 1):
         x1 = [point[0][0], point[j][0]]
         y1 = [point[0][1], point[j][1]]
-        if j == 9 and maxVel: # max velocity make red
-            ax.plot(x1, y1, color='r', linewidth=1.0, transform=ax.transAxes)
-        else:
-            ax.plot(x1, y1, color='k', linewidth=0.5, transform=ax.transAxes)
+        ax.plot(x1, y1, color='k', linewidth=lw, transform=ax.transAxes)
 
-def addVelocityValues(ax, dataPoint, point, maxVel=''):
+    if maxVel:
+        # draw red line for max velocity
+        xdIntervall = max(ax.get_xlim()) - min(ax.get_xlim())
+        ydIntervall = max(ax.get_ylim()) - min(ax.get_ylim())
+        vDiag = (ydIntervall / xdIntervall)
+        pVmax = getMaxVelocityPoint(width, height, maxVel, vDiag)
+        lVmax = ax.plot([point[0][0], pVmax[0]], [point[0][1], pVmax[1]], color='r', linewidth=lw*1.5,
+                         transform=ax.transAxes)[0]  # return handle not list
+
+        return lVmax
+
+
+def addVelocityValues(ax, dataPoint, point, first=True, inititalNrOfTextElements=np.nan):
     """ add velocity values as labels
 
         Parameters
@@ -390,45 +471,50 @@ def addVelocityValues(ax, dataPoint, point, maxVel=''):
             list of points coordinates for velocity label locations
         point: list
             list of points coordinates for velocity lines
-        maxVel: float
-            max velocity
+        first: bool
+            if True text elements are created - initially required, if False then only changed
+        inititalNrOfTextElements:
+
     """
 
+    # compute velocity values
     velocity = []
     for i in range(2, len(dataPoint)-1):
         deltaS = dataPoint[0][1] - dataPoint[i][1]
         deltaT = dataPoint[i][0] - dataPoint[0][0]
         velocity.append(deltaS/deltaT)
 
+    textElementNr = inititalNrOfTextElements
     # add velocity labels
     for j in range(2, len(point)-1):
         x1 = [point[0][0], point[j][0]]
         y1 = [point[0][1], point[j][1]]
 
-        if j < 5: # text along vertical axes
-            ax.text(x1[1]+0.01, y1[1]+0.002, '%.1f' % velocity[j-2], size=5, fontweight='bold',
-                color='k', transform=ax.transAxes, gid=j, va='center',ha='left')
-        elif j == 9 and maxVel != '': # text for max velocity
-            if velocity[j-2] < velocity[3]:
-                xLoc = x1[1]-0.04
-                yLoc =  y1[1]-0.004
-            else:
-                xLoc = x1[1]
-                yLoc =  y1[1]+0.004
-            ax.text(xLoc, yLoc, 'max %.1f' % velocity[j-2], size=5, fontweight='bold',
-                color='r', transform=ax.transAxes, gid=j, va='center',ha='left',
-                bbox=dict(facecolor='white', alpha=0.5))
-            if abs(velocity[j-2] - maxVel) > 1.e7:
-                log.warning('velocity legend wrong')
+        textElementNr = textElementNr + 1
+        if first:
+            if j < 5: # text along vertical axes
+                ax.text(x1[1]+0.01, y1[1]+0.002, '%.1f' % velocity[j-2], size=5, fontweight='bold',
+                    color='k', transform=ax.transAxes, gid=j, va='center',ha='left')
+            else: # text along horizontal axes
+                ax.text(x1[1], y1[1]-0.01, '%.1f' % velocity[j-2], size=5, fontweight='bold',
+                    color='k', transform=ax.transAxes, gid=j, va='top', ha='center')
+        else:
+            ax.texts[textElementNr].set_text('%.1f' % velocity[j - 2])
 
-        else: # text along horizontal axes
-            ax.text(x1[1], y1[1]-0.01, '%.1f' % velocity[j-2], size=5, fontweight='bold',
-                color='k', transform=ax.transAxes, gid=j, va='top', ha='center')
+    return textElementNr
 
 
-
-def getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval, maxVel, kPlot):
+def getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval):
     """ get points for legend creation (box and sloping lines)
+
+        if xMax, yMax, xinterval, yinterval are all 1 -> points refer to
+        axes coordinates, that range from 0 to 1
+
+        if given true values, e.g. in seconds and meters, points are in
+        data coordinates and can be used for velocity calculus etc.
+
+        Note: used to be an internal function to rangeTimeVelocityLegend,
+              so use with care.
 
         Parameters
         -----------
@@ -436,6 +522,14 @@ def getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval, maxVel, k
             max x extent of data (timeSteps)
         yMax: float
             max y extent of data (rangeGates)
+        width: float
+            fractional percentage of legend width
+        height: float
+            fractional percentage of legend height
+        xinterval: float
+            interval on x
+        yinterval: float
+            interval on y
 
         Returns
         --------
@@ -444,27 +538,19 @@ def getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval, maxVel, k
     """
 
     points = []
-    points.append([xMax - width*xinterval, yMax])
-    points.append([xMax, yMax])
-    points.append([xMax, yMax - height/4.*yinterval])
-    points.append([xMax, yMax - height/2.*yinterval])
-    points.append([xMax, yMax - height/4.*3.*yinterval])
-    points.append([xMax, yMax - height*yinterval])
-    points.append([xMax - width/4*xinterval, yMax - height*yinterval])
-    points.append([xMax - width/2*xinterval, yMax - height*yinterval])
-    points.append([xMax - width/4*3*xinterval, yMax - height*yinterval])
 
-    if np.isnan(maxVel):
-        points.append([np.nan, np.nan])
-    elif maxVel > kPlot:
-        points.append([(xMax - width*xinterval) + width*xinterval/maxVel*kPlot, yMax-height*yinterval])
-    else:
-        points.append([xMax, yMax - height*yinterval*maxVel/kPlot])
-
-    points.append([xMax - width*xinterval, yMax - height*yinterval])
+    points.append([xMax - width*xinterval, yMax])  # p0 left, top
+    points.append([xMax, yMax])  # p1 right, top
+    points.append([xMax, yMax - height/4.*yinterval])  # p2 right, 0.75*top
+    points.append([xMax, yMax - height/2.*yinterval])  # p3 right, 0.5*top
+    points.append([xMax, yMax - height/4.*3.*yinterval])  # p4 right, 0.25*top
+    points.append([xMax, yMax - height*yinterval])  # p5 right, bottom
+    points.append([xMax - width/4*xinterval, yMax - height*yinterval])  # p6 0.75*right, bottom
+    points.append([xMax - width/2*xinterval, yMax - height*yinterval])  # p7 0.5*right, bottom
+    points.append([xMax - width/4*3*xinterval, yMax - height*yinterval])  # p8 0.25*right, bottom
+    points.append([xMax - width*xinterval, yMax - height*yinterval])  # p9 left, bottom
 
     return points
-
 
 
 def animationPlot(demData, data, cellSize, resType, cfgRangeTime, mtiInfo, timeStep):
@@ -643,9 +729,6 @@ def animationPlot(demData, data, cellSize, resType, cfgRangeTime, mtiInfo, timeS
     timeList = mtiInfo['timeList']
     rangeList = mtiInfo['rangeList']
 
-    # compute approach velocity - required for approach velocity legend
-    maxVel, rangeVel, timeVel = dtAna.approachVelocity(mtiInfo, cfgRangeTime['GENERAL'].getfloat('minVelTimeStep'))
-
     # in case time steps are not ordered - the colormesh x and y need to be ordered
     timeIndex = np.argsort(np.array(timeList))
     timeListNew = np.array(timeList)[timeIndex]
@@ -676,16 +759,16 @@ def animationPlot(demData, data, cellSize, resType, cfgRangeTime, mtiInfo, timeS
     kPlot = (yinterval / xinterval)
 
     # fetch points for drawing velocity legend
-    point = getVelocityPoints(1, 1, width, height, 1, 1, np.nan, kPlot)
+    point = getVelocityPoints(1, 1, width, height, 1, 1)
 
     # add title and boundary box and diagonal lines using point
-    addTitleBox(ax3, lw, point, maxVel=False)
+    lVmax = addTitleBox(ax3, width, height, lw, point, textsize, maxVel=False)
 
     # compute velocity values
-    dataPoint = getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval, np.nan, kPlot)
+    dataPoint = getVelocityPoints(xMax, yMax, width, height, xinterval, yinterval)
 
     # add velocity legend labels
-    addVelocityValues(ax3, dataPoint, point, maxVel='')
+    _ = addVelocityValues(ax3, dataPoint, point)
 
     # set limits for plot (depends on final time step)
     ax3.set_ylim([yMin, yMax])

avaframe/tests/test_distanceTimeAnalysis.py
@@ -1,15 +1,12 @@
 ''' Tests for module distanceTimeAnalysis  '''
 import numpy as np
-import numpy.ma as ma
-import pandas as pd
 import pathlib
+import pytest
 import configparser
-import matplotlib.pyplot as plt
 
 # Local imports
 import avaframe.ana5Utils.distanceTimeAnalysis as dtAna
 import avaframe.in2Trans.ascUtils as IOf
-from avaframe.in3Utils import cfgUtils
 
 
 def test_getRadarLocation():
@@ -24,7 +21,7 @@ def test_getRadarLocation():
 
     assert radarFov[0][0] == 10.
     assert radarFov[1][1] == 57.
-    assert np.array_equal(radarFov, np.asarray([[10., 25.],[40., 57.]]))
+    assert np.array_equal(radarFov, np.asarray([[10., 25.], [40., 57.]]))
 
 
 def test_setDEMOrigin():
@@ -32,9 +29,8 @@ def test_setDEMOrigin():
 
     # setup required inputs
     headerDEM = {'xllcenter': 0.0, 'yllcenter': 0.0, 'cellsize': 2., 'ncols': 8, 'nrows': 11}
-    demSims = {'header': headerDEM, 'originalHeader': {'xllcenter': 1.0, 'yllcenter': 5.,
-        'cellsize': 2., 'ncols': 8, 'nrows': 11},
-        'rasterData': np.zeros((11, 8))}
+    demSims = {'header': headerDEM, 'originalHeader': {'xllcenter': 1.0, 'yllcenter': 5., 'cellsize': 2.,
+               'ncols': 8, 'nrows': 11}, 'rasterData': np.zeros((11, 8))}
 
     # call function to be tested
     demOriginal = dtAna.setDemOrigin(demSims)
@@ -56,15 +52,13 @@ def test_radarMask(tmp_path):
     radarFov = [[1., 10.0], [5., 5.]]
     aperture = 40.5
     cfgRangeTime = configparser.ConfigParser()
-    cfgRangeTime['GENERAL'] = {'rgWidth': 2., 'avalancheDir': testAvaDir, 'simHash': 'test123',
-        'gateContours': 20}
+    cfgRangeTime['GENERAL'] = {'rgWidth': 2., 'avalancheDir': testAvaDir, 'simHash': 'test123', 'gateContours': 20}
 
     # call function to be tested
     radarRange, rangeGates = dtAna.radarMask(demOriginal, radarFov, aperture, cfgRangeTime)
 
     print('randarRange', radarRange)
     print('rangeGates', rangeGates)
-
 
     assert np.array_equal(rangeGates, np.asarray([2., 4., 6., 8., 10.]))
 
@@ -77,8 +71,7 @@ def setupRangeTimeDiagram():
     demOriginal = {'header': headerDEM, 'rasterData': np.zeros((11, 11))}
 
     cfgRangeTime = configparser.ConfigParser()
-    cfgRangeTime['GENERAL'] = {'rgWidth': 2., 'simHash': 'test123',
-        'gateContours': 20, 'aperture': 40.5}
+    cfgRangeTime['GENERAL'] = {'rgWidth': 2., 'simHash': 'test123', 'gateContours': 20, 'aperture': 40.5}
 
     # call function to be tested
     mtiInfo = dtAna.setupRangeTimeDiagram(demOriginal, cfgRangeTime)
@@ -107,14 +100,26 @@ def test_appraoachVelocity():
 
     # setup required inputs
     mtiInfo = {'timeList': [0., 2., 3., 7., 8., 9., 10.],
-        'rangeList': [0., 2., 4., 6., 8., 10., 12]}
+               'rangeList': [0., 2., 4., 4., 7., 10., 12]}
     minVelTimeStep = 2.
 
     # call function to be tested
-    maxVel, rangeVel, timeVel = dtAna.approachVelocity(mtiInfo, minVelTimeStep)
+    maxVel, rangeVel, timeVel = dtAna.approachVelocity(mtiInfo)
+    print(maxVel, rangeVel, timeVel)
+    assert maxVel == pytest.approx(1.0, rel=1e-8)
+    assert rangeVel == 0.
+    assert timeVel == 0.
 
-    assert maxVel == 2.
-    assert rangeVel == 8.
+    # setup required inputs
+    mtiInfo = {'timeList': [0., 2., 3., 7., 8., 9., 10.],
+               'rangeList': [0., np.nan, 4., 6., 7., 10., 12]}
+    minVelTimeStep = 2.
+
+    # call function to be tested
+    maxVel, rangeVel, timeVel = dtAna.approachVelocity(mtiInfo)
+    print(maxVel, rangeVel, timeVel)
+    assert maxVel == pytest.approx(3.0, rel=1e-8)
+    assert rangeVel == 7.
     assert timeVel == 8.
 
 
@@ -132,7 +137,7 @@ def test_fetchTimeStepFromName():
 
     # setup required inpu
     pathNames = [pathlib.Path('tests', 'testName_t0.5.asc'),
-        pathlib.Path('tests', 'testName_t0.15.asc')]
+                 pathlib.Path('tests', 'testName_t0.15.asc')]
 
     # call function to be tested
     timeSteps, indexTime = dtAna.fetchTimeStepFromName(pathNames)
@@ -203,13 +208,14 @@ def test_maskRangeFull():
                           [True, True, False, False],
                           [True, True, False, False]])
     maskAvaSol = np.asarray([[True, True, True, True],
-                          [True, False, False, False],
-                          [False, True, True, True],
-                          [False, False, False, False],
-                          [True, False, False, False]])
+                            [True, False, False, False],
+                            [False, True, True, True],
+                            [False, False, False, False],
+                            [True, False, False, False]])
 
     assert np.array_equal(maskSol, maskFull)
     assert np.array_equal(maskAvaSol, maskAva.mask)
+
 
 def test_extractFrontAndMeanValuesRadar():
     """ test extracting front and mean values """
@@ -223,12 +229,11 @@ def test_extractFrontAndMeanValuesRadar():
                         [7., 2., 0., 0.],
                         [6., 7., 8., 10.],
                         [2., 4., 5., 7.]])
-    threshold = 3.1
     range = np.arange(4)
     range = np.repeat([range], 5, axis=0)
     rangeMasked = np.ma.masked_where(range < 2., range)
     mtiInfo = {'rangeGates': np.arange(4), 'rangeMasked': rangeMasked, 'rArray': range,
-    'mti': np.zeros(5), 'rangeList': [], 'timeList': []}
+               'mti': np.zeros(5), 'rangeList': [], 'timeList': []}
 
     # call function to be tested
     mtiInfo = dtAna.extractFrontAndMeanValuesRadar(cfgRangeTime, flowF, mtiInfo)
@@ -248,7 +253,6 @@ def test_setupThalwegTimeDiagram():
     # setup required inputs
     dirPath = pathlib.Path(__file__).parents[0]
     avaDir = dirPath / '..' / 'data/avaInclinedPlane'
-    modName = 'com1DFA'
     inputDir = dirPath / '..' / avaDir / 'Inputs'
     demPath = inputDir / 'DEM_IP_Topo.asc'
     dem = IOf.readRaster(demPath)
@@ -262,7 +266,7 @@ def test_setupThalwegTimeDiagram():
     mtiInfo = dtAna.setupThalwegTimeDiagram(dem, cfgRangeTime)
 
     print('mtiInfo', mtiInfo['betaPoint'], mtiInfo['betaPointAngle'],
-    mtiInfo['type'], np.amin(mtiInfo['rangeGates']), np.amax(mtiInfo['rangeGates']))
+          mtiInfo['type'], np.amin(mtiInfo['rangeGates']), np.amax(mtiInfo['rangeGates']))
 
     assert len(mtiInfo['rangeGates']) == len(mtiInfo['rasterTransfo']['s'])
     assert np.isclose(mtiInfo['betaPoint'][1], -4000.)
@@ -290,7 +294,7 @@ def test_initializeRangeTime():
     # call function to be tested
     mtiInfo, dtRangeTime, cfgRangeTime = dtAna.initializeRangeTime(dtAna, cfg, dem, simHash)
 
-    #print('mtiInfo', mtiInfo)
+    # print('mtiInfo', mtiInfo)
     print('dtRa', dtRangeTime, mtiInfo['rasterTransfo']['startOfRunoutAreaAngle'])
 
     assert np.isclose(mtiInfo['rasterTransfo']['startOfRunoutAreaAngle'], 9.95742856)
@@ -317,15 +321,15 @@ def test_fetchRangeTimeInfo():
     mtiInfo, dtRangeTime, cfgRangeTime = dtAna.initializeRangeTime(dtAna, cfg, dem, simHash)
 
     fieldTest = np.ones((dem['rasterData'].shape))
-    fields = {'FT': fieldTest}
+    fields = {'FV': fieldTest}
 
     # call function to be tested
     mtiInfo, dtRangeTime = dtAna.fetchRangeTimeInfo(cfgRangeTime, cfg, dtRangeTime, 1., dem['header'], fields,
-        mtiInfo)
+                                                    mtiInfo)
 
     print('mtiInfo', type(dtRangeTime))
 
-    assert np.array_equal(dtRangeTime, np.arange(2,40,1))
+    assert np.array_equal(dtRangeTime, np.arange(2, 40, 1))
 
 
 def test_extractFrontAndMeanValuesTT():
@@ -354,7 +358,6 @@ def test_extractFrontAndMeanValuesTT():
 
     # call function to be tested
     mtiInfo = dtAna.extractFrontAndMeanValuesTT(cfgRangeTime, flowF, dem['header'], mtiInfo)
-
 
     print('mtiInfo', mtiInfo['rangeList'], mtiInfo['mti'])
     print('raster', mtiInfo['rasterTransfo']['startOfRunoutAreaAngle'])