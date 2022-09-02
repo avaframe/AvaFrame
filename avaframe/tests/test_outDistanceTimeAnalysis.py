0afca0f04f300d2b15dc176c881a863b8e512ed3
Anna Wirbel <anna@avaframe.org>
2022-09-02 14:21:05 +0200

    update pytest

--- avaframe/tests/test_outDistanceTimeAnalysis.py
+++ avaframe/tests/test_outDistanceTimeAnalysis.py
@@ -48,3 +48,40 @@ def test_radarFieldOfViewPlot(tmp_path):
     dem = {'header': {'nrows': 45, 'ncols': 50, 'cellsize': 5., 'xllcenter': 0.0, 'yllcenter': 0.0}, 'rasterData': demData}
 
     oAna.radarFieldOfViewPlot(radarFov, radarRange, cfgRangeTime, rangeGates, dem)
+
+
+def test_animationPlot(tmp_path):
+    """ test plot range time """
+
+    # setup required inputs
+    avaDir = pathlib.Path(tmp_path)
+    mti = np.repeat([np.arange(50)], 45, axis=0)
+    demData = {'rasterData': np.zeros((10, 15)), 'header': {'cellsize': 5., 'xllcenter': 0.0,
+        'yllcenter': 0.0, 'noDataValue': 9999, 'nrows': 10, 'ncols': 15}}
+    data = np.ones((10, 15))
+    mtiInfo = {'mti': mti, 'rangeList': np.arange(50), 'timeList': np.arange(50),
+        'rangeGates': np.arange(45) -25., 'plotTitle': 'testTitle', 'referencePointName': 'betaPoint',
+        'type': 'tt', 'betaPointAngle': 9.987, 'cLower': 3, 'slRaster': np.ones((45,45)),
+        'rangeRaster': np.ones((50, 45)), 'sType': 'projected'}
+    mtiInfo['rasterTransfo'] = {'x': np.arange(45), 'y': np.ones(45), 'cellSizeSL': 2., 'DBXl': 0.0,
+        'DBXr': 2., 'DBYl': 0, 'DBYr': 2., 'indStartOfRunout': 4, 'gridx': np.ones((10, 15)),
+        'gridy': np.ones((10, 15)), 'l': np.arange(45), 's': np.arange(45)}
+
+    cfgRangeTime = configparser.ConfigParser()
+    cfgRangeTime['GENERAL'] = {'avalancheDir': avaDir, 'rangeTimeResType': 'FT', 'simHash': 'simDI',
+        'minVelTimeStep': 2., 'thresholdResult': 1.e-2, 'minVelTimeStep': 2, 'maxOrMean': 'max'}
+    cfgRangeTime['PLOTS'] = {'width': 0.25, 'height': 0.25, 'lw': 0.25, 'textsize': 7}
+    cfgRangeTime['ANIMATE'] = {'resMin': 0.0, 'resMax': 10., 'xyEastNorth': True, 'panelTitles': True,
+        'xMin': 0.0, 'xMax': 45, 'yMin': 0.0, 'yMax': 45.}
+    cellSize = 5.
+    resType = 'FT'
+    timeStep = 1.
+
+    # call function to be tested
+    oAna.animationPlot(demData, data, cellSize, resType, cfgRangeTime, mtiInfo, timeStep)
+
+    cfgRangeTime['ANIMATE']['panelTitles'] = 'False'
+    cfgRangeTime['ANIMATE']['xyEastNorth'] = 'False'
+
+    # call function to be tested
+    oAna.animationPlot(demData, data, cellSize, resType, cfgRangeTime, mtiInfo, timeStep)
