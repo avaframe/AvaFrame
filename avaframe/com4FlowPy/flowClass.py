626840aad9713f7244f2663f1e5cde5319954a5c
Felix Oesterle <felix@avaframe.org>
2022-09-06 09:36:52 +0200

    Remove requirements from com4FlowPy; add rasterio to avaframe
     - Remove modules folder (was related to flowpy helpers)

--- avaframe/com4FlowPy/flowClass.py
+++ avaframe/com4FlowPy/flowClass.py
@@ -7,19 +7,6 @@ This is the flow class
 
     Copyright (C) <2020>  <Michael Neuhauser>
     Michael.Neuhauser@bfw.gv.at
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import numpy as np
@@ -28,7 +15,9 @@ import math
 
 class Cell:
     
-    def __init__(self, rowindex, colindex, dem_ng, cellsize, flux, z_delta, parent, alpha, exp, flux_threshold, max_z_delta, startcell):
+    def __init__(self, rowindex, colindex, dem_ng, cellsize, flux, 
+                 z_delta, parent, alpha, exp, flux_threshold, 
+                 max_z_delta, startcell):
         '''This class handles the spreading over the DEM!
         Depending on the process different alpha angles are used for energy dissipation.'''
         self.rowindex = rowindex
--- avaframe/com4FlowPy/flowCore.py
+++ avaframe/com4FlowPy/flowCore.py
@@ -15,25 +15,13 @@ of DEM, return arrays)
     Copyright (C) <2020>  <Michael Neuhauser>
     Michael.Neuhauser@bfw.gv.at
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import sys
 import numpy as np
 from datetime import datetime
 import logging
-from avaframe.com4FlowPy.flow_class import Cell
+from avaframe.com4FlowPy.flowClass import Cell
 
 
 def get_start_idx(dem, release):
