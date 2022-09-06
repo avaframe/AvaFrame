126de4969134ad4e366b87b7b8b89d88aeb76c00
Felix Oesterle <felix@avaframe.org>
2022-09-06 09:36:52 +0200

    Remove requirements from com4FlowPy; add rasterio to avaframe
     - Remove modules folder (was related to flowpy helpers)
     - Fix missing import; code clean
    
     squash

--- avaframe/com4FlowPy/rasterIo.py
+++ avaframe/com4FlowPy/rasterIo.py
@@ -5,22 +5,9 @@ Created on Tue Jun 11 18:44:52 2019
 @author: Michael Neuhauser
 This file is for reading and writing raster files
 
-
     Copyright (C) <2020>  <Michael Neuhauser>
     Michael.Neuhauser@bfw.gv.at
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import rasterio
@@ -79,8 +66,8 @@ def output_raster(file, file_out, raster):
     new_dataset.close()
 
 
-#path = '/home/lawinenforschung/Desktop/PAR6_ValsGries_AUT/dhm_10_6.tif'
-#output = 'example.asc'
-#raster, header = read_raster(path)
-#output_raster(path, output, raster)
-#rasterio.rio.convert(output, 'example.asc', driver='AAIGrid')
+# path = '/home/lawinenforschung/Desktop/PAR6_ValsGries_AUT/dhm_10_6.tif'
+# output = 'example.asc'
+# raster, header = read_raster(path)
+# output_raster(path, output, raster)
+# rasterio.rio.convert(output, 'example.asc', driver='AAIGrid')
