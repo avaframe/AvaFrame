aa4444194bdc2d8c44a077ce8a760d7e8ac1a8d5
Anna Wirbel <anna@avaframe.org>
2023-01-25 13:17:47 +0100

    udpate dam break

--- avaframe/runScripts/runDamBreak.py
+++ avaframe/runScripts/runDamBreak.py
@@ -42,3 +42,5 @@ for sphKernelRadius in [5, 3]:
 
     # call com1DFA to perform simulation - provide configuration file and release thickness function
     dem, plotDict, reportDictList, simDF = com1DFA.com1DFAMain(cfgMain, cfgInfo=damBreakCfg)
+    # Clean input directory(ies) of old work 
+    initProj.cleanSingleAvaDir(avalancheDir, keep=logName, deleteOutput=False)
