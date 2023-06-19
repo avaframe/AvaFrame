f7f2697de016e3a7ff5434b8038ef8e1b4f47715
Felix Oesterle <felix@avaframe.org>
2023-06-19 13:04:20 +0200

    Change loging of cleanSingleAvaDir -> keep logs [in3Utils]
      - Perpare com1dfa com2AB runscripts for QGis_AF

--- avaframe/runCom1DFA.py
+++ avaframe/runCom1DFA.py
@@ -1,30 +1,83 @@
 """
-    Run script for running python DFA kernel
+    Run script for running python com1DFA kernel
 """
+# Load modules
+# importing general python modules
+import time
+import argparse
+import pathlib
+
 
 # Local imports
 import avaframe.in3Utils.initializeProject as initProj
 from avaframe.com1DFA import com1DFA
 from avaframe.in3Utils import cfgUtils
 from avaframe.in3Utils import logUtils
+from avaframe.in3Utils import fileHandlerUtils as fU
 
 
-""" run com1DFA module """
+def runCom1DFA(avalancheDir=''):
+    """ Run com1DFA in the default configuration with only an
+    avalanche directory as input
 
-# +++++++++SETUP CONFIGURATION++++++++++++++++++++++++
-# log file name; leave empty to use default runLog.log
-logName = 'runCom1DFA'
+    Parameters
+    ----------
+    avalancheDir: str
+        path to avalanche directory (setup eg. with init scipts)
 
-# Load avalanche directory from general configuration file
-cfgMain = cfgUtils.getGeneralConfig()
-avalancheDir = cfgMain['MAIN']['avalancheDir']
+    Returns
+    -------
+    peakFilesDF: pandas dataframe
+        dataframe with info about com1DFA peak file locations
+    """
 
-# Clean input directory of old work and output files from module
-initProj.cleanModuleFiles(avalancheDir, com1DFA, deleteOutput=False)
-# Start logging
-log = logUtils.initiateLogger(avalancheDir, logName)
-log.info('MAIN SCRIPT')
-log.info('Current avalanche: %s', avalancheDir)
+    # Time the whole routine
+    startTime = time.time()
 
-# call com1DFA and perform simulations
-dem, plotDict, reportDictList, simDF = com1DFA.com1DFAMain(cfgMain, cfgInfo='')
+    # log file name; leave empty to use default runLog.log
+    logName = 'runCom1DFA'
+
+    # Load avalanche directory from general configuration file
+    # More information about the configuration can be found here
+    # on the Configuration page in the documentation
+    cfgMain = cfgUtils.getGeneralConfig()
+    if avalancheDir != '':
+        cfgMain['MAIN']['avalancheDir'] = avalancheDir
+    else:
+        avalancheDir = cfgMain['MAIN']['avalancheDir']
+
+    # Start logging
+    log = logUtils.initiateLogger(avalancheDir, logName)
+    log.info('MAIN SCRIPT')
+    log.info('Current avalanche: %s', avalancheDir)
+
+    # ----------------
+    # Clean input directory(ies) of old work and output files
+    # If you just created the ``avalancheDir`` this one should be clean but if you
+    # already did some calculations you might want to clean it::
+    initProj.cleanSingleAvaDir(avalancheDir, keep=logName, deleteOutput=False)
+
+    # ----------------
+    # Run dense flow
+    dem, plotDict, reportDictList, simDF = com1DFA.com1DFAMain(cfgMain, cfgInfo='')
+
+    # Get peakfiles to return to QGIS
+    avaDir = pathlib.Path(avalancheDir)
+    inputDir = avaDir / 'Outputs' / 'com1DFA' / 'peakFiles'
+    peakFilesDF = fU.makeSimDF(inputDir, avaDir=avaDir)
+
+    # Print time needed
+    endTime = time.time()
+    log.info('Took %6.1f seconds to calculate.' % (endTime - startTime))
+
+    return peakFilesDF
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Run com1DFA workflow')
+    parser.add_argument('avadir', metavar='a', type=str, nargs='?', default='',
+                        help='the avalanche directory')
+
+    args = parser.parse_args()
+    runCom1DFA(str(args.avadir))
