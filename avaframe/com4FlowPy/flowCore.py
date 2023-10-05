5dbf85327d947111ae2e561530b76ed55e1acc95
Neuhauser Michael <michael.neuhauser@bfw.gv.at>
2023-10-05 09:09:12 +0200

    Bring in latest flowPy develop
      - enabled mutliprocessing, but bug in release split or so
      - changed release_split and found bug, ToDo: make this usable
      - updated split_release function, probably killed bug
      - changed flowpy .ini, changed rasterio so output is in dem porjection or None

--- avaframe/com4FlowPy/flowCore.py
+++ avaframe/com4FlowPy/flowCore.py
@@ -19,8 +19,16 @@ of DEM, return arrays)
 
 import sys
 import numpy as np
-from datetime import datetime
 import logging
+import os
+import platform
+if os.name == 'nt':
+    from multiprocessing.pool import Pool as Pool    
+elif platform.system() == 'Darwin':
+    from multiprocessing.pool import ThreadPool as Pool
+else:
+    from multiprocessing import Pool
+
 from avaframe.com4FlowPy.flowClass import Cell
 
 
@@ -45,6 +53,47 @@ def get_start_idx(dem, release):
         # Sort this lists by altitude
     return row_list, col_list
 
+def split_release(release, pieces):
+    """Split the release layer in several tiles, the number is depending on the
+    available CPU Cores, so every Core gets one tile. The area is determined by
+    the number of release pixels in it, so that every tile has the same amount
+    of release pixels in it. Splitting in x(Columns) direction.
+    The release tiles have still the size of the original layer, so no split
+    for the DEM is needed.
+
+    Input parameters:
+        release         the release layer with release pixels as int > 0
+        header_release  the header of the release layer to identify the
+                        noDataValue
+
+    Output parameters:
+        release_list    A list with the tiles(arrays) in it [array0, array1, ..]
+        """
+
+    release[release < 0] = 0
+    release[release > 1] = 1
+    sumRelease = np.sum(release) # Count number of release pixels
+    sum_per_split = sumRelease/pieces  # Divide the number by avaiable Cores
+    release_list = []
+    #2022-09-06 - AH: Note - maybe we need to think of sth. smarter here!!
+    #i.e. not slicing in columns??    
+    breakpoint_x = 0
+    for i in range(release.shape[1]):
+        if (len(release_list) == (pieces - 1)) or (np.sum(release[:, i:]) <= sum_per_split):
+            c = np.zeros_like(release)
+            c[:, breakpoint_x:] = release[:, breakpoint_x:]
+            release_list.append(c)
+            break
+        if np.sum(release[:, breakpoint_x:i]) < sum_per_split:
+            continue
+        else:
+            c = np.zeros_like(release)
+            c[:, breakpoint_x:i] = release[:, breakpoint_x:i]
+            release_list.append(c)
+            print("Release Split from {} to {}".format(breakpoint_x, i))
+            breakpoint_x = i
+    
+    return release_list
 
 def back_calculation(back_cell):
     """Here the back calculation from a run out pixel that hits a infrastructure
@@ -74,16 +123,92 @@ def back_calculation(back_cell):
     #print('\n Backcalculation needed: ' + str(end - start) + ' seconds')
     return back_list
 
+def run(optTuple):
+    
+    log = logging.getLogger(__name__)
+    tempDir = optTuple[8]
+    infraBool = optTuple[9]
+    nCPU = optTuple[10]
 
-def calculation(optTuple):
+    dem = np.load(tempDir / ("dem_%s_%s.npy" % (optTuple[0], optTuple[1])))
+    release = np.load(tempDir / ("init_%s_%s.npy" % (optTuple[0], optTuple[1])))
+    if infraBool:
+        infra = np.load(tempDir / ("infra_%s_%s.npy" % (optTuple[0], optTuple[1])))
+    else:
+        infra = np.zeros_like(dem)
+
+    alpha = float(optTuple[2])
+    exp = float(optTuple[3])
+    cellsize = float(optTuple[4])
+    nodata = float(optTuple[5])
+    flux_threshold = float(optTuple[6])
+    max_z_delta = float(optTuple[7])
+
+    log.info("Multiprocessing starts, used cores: %i" % (nCPU))
+        
+    release_list = split_release(release, nCPU)
+    
+    with Pool(processes=nCPU) as pool:
+        results = pool.map(calculation,[[dem, infra, release_sub, alpha, exp, flux_threshold, max_z_delta, nodata, cellsize, infraBool]
+                            for release_sub in release_list])
+        pool.close()
+        pool.join()
+
+    z_delta_array = np.zeros_like(dem)
+    flux_array = np.zeros_like(dem)
+    count_array = np.zeros_like(dem)
+    z_delta_sum = np.zeros_like(dem)
+    backcalc = np.zeros_like(dem)
+    fp_travelangle_array = np.zeros_like(dem)
+    sl_travelangle_array = np.zeros_like(dem)
+
+    z_delta_list = []
+    flux_list = []
+    cc_list = []
+    z_delta_sum_list = []
+    backcalc_list = []
+    fp_ta_list = []
+    sl_ta_list = []
+    
+    for i in range(len(results)):
+        res = results[i]
+        res = list(res)
+        z_delta_list.append(res[0])
+        flux_list.append(res[1])
+        cc_list.append(res[2])
+        z_delta_sum_list.append(res[3])
+        backcalc_list.append(res[4])
+        fp_ta_list.append(res[5])
+        sl_ta_list.append(res[6])
+
+    logging.info('Calculation finished, getting results.')
+    for i in range(len(z_delta_list)):
+        z_delta_array = np.maximum(z_delta_array, z_delta_list[i])
+        flux_array = np.maximum(flux_array, flux_list[i])
+        count_array += cc_list[i]
+        z_delta_sum += z_delta_sum_list[i]
+        backcalc = np.maximum(backcalc, backcalc_list[i])
+        fp_travelangle_array = np.maximum(fp_travelangle_array, fp_ta_list[i])
+        sl_travelangle_array = np.maximum(sl_travelangle_array, sl_ta_list[i])        
+        
+    # Save Calculated tiles
+    np.save(tempDir / ("res_z_delta_%s_%s" % (optTuple[0], optTuple[1])), z_delta_array)
+    np.save(tempDir / ("res_z_delta_sum_%s_%s" % (optTuple[0], optTuple[1])), z_delta_sum)
+    np.save(tempDir / ("res_flux_%s_%s" % (optTuple[0], optTuple[1])), flux_array)
+    np.save(tempDir / ("res_count_%s_%s" % (optTuple[0], optTuple[1])), count_array)
+    np.save(tempDir / ("res_fp_%s_%s" % (optTuple[0], optTuple[1])), fp_travelangle_array)
+    np.save(tempDir / ("res_sl_%s_%s" % (optTuple[0], optTuple[1])), sl_travelangle_array)
+    if infraBool:
+        np.save(tempDir / ("res_backcalc_%s_%s" % (optTuple[0], optTuple[1])), backcalc)
+
+def calculation(args):
     """This is the core function where all the data handling and calculation is
     done.
 
     Input parameters:
         dem         The digital elevation model
         header      The header of the elevation model
-        forest      The forest layer
-        process     Which process to calculate (Avalanche, Rockfall, SoilSlides)
+        infra       The infra layer
         release     The list of release arrays
         alpha
         exp
@@ -98,20 +223,17 @@ def calculation(optTuple):
         elh_sum     Array with the sum of Energy Line Height
         back_calc   Array with back calculation, still to do!!!
         """
-    tempDir = optTuple[8]
-    infraBool = optTuple[9]
-
-    dem = np.load(tempDir / ("dem_%s_%s.npy" % (optTuple[0], optTuple[1])))
-    release = np.load(tempDir / ("init_%s_%s.npy" % (optTuple[0], optTuple[1])))
-    if infraBool:
-        infra = np.load(tempDir / ("infra_%s_%s.npy" % (optTuple[0], optTuple[1])))
-
-    alpha = float(optTuple[2])
-    exp = float(optTuple[3])
-    cellsize = float(optTuple[4])
-    nodata = float(optTuple[5])
-    flux_threshold = float(optTuple[6])
-    max_z_delta = float(optTuple[7])
+    
+    dem = args[0]
+    infra = args[1]
+    release = args[2]
+    alpha = args[3]
+    exp = args[4]
+    flux_threshold = args[5]
+    max_z_delta=args[6]
+    nodata = args[7]
+    cellsize = args[8]
+    infraBool = args[9]
 
     z_delta_array = np.zeros_like(dem, dtype=np.float32)
     z_delta_sum = np.zeros_like(dem, dtype=np.float32)
@@ -121,12 +243,13 @@ def calculation(optTuple):
     fp_travelangle_array = np.zeros_like(dem, dtype=np.float32)  # fp = Flow Path
     sl_travelangle_array = np.zeros_like(dem, dtype=np.float32) * 90  # sl = Straight Line
     
-    if infraBool:
-        backcalc = np.zeros_like(dem, dtype=np.int32)
+    backcalc = np.zeros_like(dem, dtype=np.int32)
+    
+    if infraBool:        
         back_list = []
 
     # Core
-    start = datetime.now().replace(microsecond=0)
+    #start = datetime.now().replace(microsecond=0)
     row_list, col_list = get_start_idx(dem, release)
 
     startcell_idx = 0
@@ -202,18 +325,7 @@ def calculation(optTuple):
             # Check if i hit a release Cell, if so set it to zero and get again the indexes of release cells
             row_list, col_list = get_start_idx(dem, release)
         startcell_idx += 1
-    end = datetime.now().replace(microsecond=0)
+    #end = datetime.now().replace(microsecond=0)
+    return z_delta_array, flux_array, count_array, z_delta_sum, backcalc, fp_travelangle_array, sl_travelangle_array
 
-    # Save Calculated tiles
-    np.save(tempDir / ("res_z_delta_%s_%s" % (optTuple[0], optTuple[1])), z_delta_array)
-    np.save(tempDir / ("res_z_delta_sum_%s_%s" % (optTuple[0], optTuple[1])), z_delta_sum)
-    np.save(tempDir / ("res_flux_%s_%s" % (optTuple[0], optTuple[1])), flux_array)
-    np.save(tempDir / ("res_count_%s_%s" % (optTuple[0], optTuple[1])), count_array)
-    np.save(tempDir / ("res_fp_%s_%s" % (optTuple[0], optTuple[1])), fp_travelangle_array)
-    np.save(tempDir / ("res_sl_%s_%s" % (optTuple[0], optTuple[1])), sl_travelangle_array)
-    if infraBool:
-        np.save(tempDir / ("res_backcalc_%s_%s" % (optTuple[0], optTuple[1])), backcalc)
-
-    #print('\n Time needed: ' + str(end - start))
-    #print("Finished calculation %s_%s" % (optTuple[0], optTuple[1]))
 
--- avaframe/runCom4FlowPy.py
+++ avaframe/runCom4FlowPy.py
@@ -8,29 +8,34 @@ from avaframe.in3Utils import cfgUtils
 from avaframe.in3Utils import logUtils
 
 
-# log file name; leave empty to use default runLog.log
-logName = 'runcom4FlowPy'
+def main():
+    # log file name; leave empty to use default runLog.log
+    logName = 'runcom4FlowPy'
+    
+    # Load avalanche directory from general configuration file
+    cfgMain = cfgUtils.getGeneralConfig()
+    avalancheDir = cfgMain['MAIN']['avalancheDir']
+    # Clean input directory of old work and output files from module
+    initProj.cleanModuleFiles(avalancheDir, com4FlowPy, deleteOutput=False)
+    
+    # Start logging
+    log = logUtils.initiateLogger(avalancheDir, logName)
+    log.info('MAIN SCRIPT')
+    log.info('Current avalanche: %s', avalancheDir)
+    
+    # Load all input Parameters from config file
+    # get the configuration of an already imported module
+    # write config to log file
+    cfg = cfgUtils.getModuleConfig(com4FlowPy)
+    
+    cfgSetup = cfg['SETUP']
+    cfgFlags = cfg['FLAGS']
+    
+    # Extract input file locations
+    cfgPath = com4FlowPy.readFlowPyinputs(avalancheDir, cfg)
+    
+    com4FlowPy.com4FlowPyMain(cfgPath, cfgSetup)
 
-# Load avalanche directory from general configuration file
-cfgMain = cfgUtils.getGeneralConfig()
-avalancheDir = cfgMain['MAIN']['avalancheDir']
-# Clean input directory of old work and output files from module
-initProj.cleanModuleFiles(avalancheDir, com4FlowPy, deleteOutput=False)
+if __name__ == '__main__':
+    main()
 
-# Start logging
-log = logUtils.initiateLogger(avalancheDir, logName)
-log.info('MAIN SCRIPT')
-log.info('Current avalanche: %s', avalancheDir)
-
-# Load all input Parameters from config file
-# get the configuration of an already imported module
-# write config to log file
-cfg = cfgUtils.getModuleConfig(com4FlowPy)
-
-cfgSetup = cfg['SETUP']
-cfgFlags = cfg['FLAGS']
-
-# Extract input file locations
-cfgPath = com4FlowPy.readFlowPyinputs(avalancheDir, cfg)
-
-com4FlowPy.com4FlowPyMain(cfgPath, cfgSetup)
