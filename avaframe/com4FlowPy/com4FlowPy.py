try to correct thickness calculation

avaframe/com4FlowPy/com4FlowPy.py
@@ -226,7 +226,8 @@ def com4FlowPyMain(cfgPath, cfgSetup):
     #Paula
     flow_energy = SPAM.MergeRaster(tempDir, "res_flow_energy")
     velocity = np.sqrt(z_delta * 2 * 9.81)
-    thickness = flux * 1.5 #height of release area is 1.5 m for ISeeSnow
+    thickness = flux * 165254 * 1.5 / cellsize**2 # real topo: height of release area is 1.5 m for ISeeSnow
+    #thickness = flux * 47972 * 1.5 / cellsize**2 # ideal topo: height of release area is 1.5 m for ISeeSnow
     #ende paula
     if infraBool:
         backcalc = SPAM.MergeRaster(tempDir, "res_backcalc")

avaframe/com4FlowPy/flowClass.py
@@ -90,7 +90,8 @@ class Cell:
         u = np.sqrt(self.z_delta * 2 * g)
         # mass = self.flux * cellsize**2 * h0 * rho
         # h = mass / cellsize**2 / rho
-        V = 165254 * 1.5 # m³ Volume #iSeeSnow IdealiedTopo
+        V = 165254 * 1.5 # m³ Volume #iSeeSnow RealTopo
+        #V = 47972 * 1.5 # m³ Volume #iSeeSnow IdealizedTopo
         h = self.flux * V / self.cellsize**2
         
         