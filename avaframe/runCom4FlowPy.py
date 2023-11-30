85e0b7901cc4de50c3a2dd0d293a37ab239f842f
ahuber-bfw <andreas.huber@bfw.gv.at>
2023-11-30 17:45:21 +0100

    added functionality to use custom input-paths and working-directories for com4FlowPy

--- avaframe/runCom4FlowPy.py
+++ avaframe/runCom4FlowPy.py
@@ -1,13 +1,17 @@
 """
 Run script for module com4FlowPy
 """
+# Load modules
+import pathlib
+import os
+from datetime import datetime
 
 # Local imports
 import avaframe.in3Utils.initializeProject as initProj
 from avaframe.com4FlowPy import com4FlowPy
 from avaframe.in3Utils import cfgUtils
 from avaframe.in3Utils import logUtils
-
+import avaframe.in3Utils.initialiseDirs as inDirs
 
 def main():
     # log file name; leave empty to use default runLog.log
@@ -18,9 +22,9 @@ def main():
 
     cfgSetup = cfg['SETUP']
     cfgFlags = cfg['FLAGS']
-    cfgPaths = cfg['PATHS']
+    cfgCustomPaths = cfg['PATHS']
 
-    if cfgPaths["useCustomPaths"]=='False':
+    if cfgCustomPaths["useCustomPaths"]=='False':
         # Load avalanche directory from general configuration file
         avalancheDir = cfgMain['MAIN']['avalancheDir']
         # Clean input directory of old work and output files from module
@@ -38,9 +42,43 @@ def main():
 
         com4FlowPy.com4FlowPyMain(cfgPath, cfgSetup)
 
-    elif cfgPaths["useCustomPaths"]=='True':
-        print('True')
+    elif cfgCustomPaths["useCustomPaths"]=='True':
+        cfgPath = {}
+
+        # Handling Custom directory creation
+        workDir = pathlib.Path(cfgCustomPaths['workDir'])
+        
+        time_string = datetime.now().strftime("%Y%m%d_%H%M%S")
+        try:
+            os.makedirs(workDir / 'res_{}'.format(time_string))
+            res_dir = (workDir / 'res_{}'.format(time_string))
+        except FileExistsError:
+            res_dir = (workDir / 'res_{}'.format(time_string))
+
+        try:
+            os.makedirs(workDir / res_dir / 'temp')
+            temp_dir = (workDir / res_dir / 'temp')
+        except FileExistsError:
+            temp_dir = (workDir / res_dir / 'temp')
+
+
+        cfgPath["workDir"]=pathlib.Path(workDir)
+        cfgPath["outDir"]=pathlib.Path(res_dir)
+        cfgPath["tempDir"]=pathlib.Path(temp_dir)
+        cfgPath["demPath"]=pathlib.Path(cfgCustomPaths["demPath"])
+        cfgPath["releasePath"]=pathlib.Path(cfgCustomPaths["releasePath"])
+        cfgPath["infraPath"]=pathlib.Path(cfgCustomPaths["infraPath"])
+
+        log = logUtils.initiateLogger(cfgPath["outDir"], logName)
+
+        cfgSetup["cpuCount"] = str(cfgUtils.getNumberOfProcesses(cfgMain,999))
+        cfgPath["customDirs"] = cfgCustomPaths["useCustomPaths"]
+
+        com4FlowPy.com4FlowPyMain(cfgPath, cfgSetup)
+
     else:
+        log.info('INPUT SETTINGS incorrect - abort')
+        sys.exit(1)
         pass
 
 if __name__ == '__main__':
