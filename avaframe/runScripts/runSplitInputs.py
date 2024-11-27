617915f953d0c3941e68815ec567c126492f4113
leon-wagner <leonwag2002@gmail.com>
2024-11-27 12:01:00 +0100

    splitting script and regional prototype progress
    - regional is now its own computational module (com7)
    - introduced configurations and overrides for in4 and com7
    - in regionalRun, peak files may now be moved to a single outDir
    - smaller changes

--- avaframe/runScripts/runSplitInputs.py
+++ avaframe/runScripts/runSplitInputs.py
@@ -5,7 +5,7 @@ import pathlib
 import argparse
 
 from avaframe.in3Utils import cfgUtils, logUtils
-from avaframe.in4Region import splitInputs as si
+from avaframe.in4Region import splitInputs as sI
 
 def runSplitInputs(avalancheDir=''):
     """Main function to split input data for each release area feature with only an avalancheDir as input."""
@@ -17,49 +17,53 @@ def runSplitInputs(avalancheDir=''):
     # Time the whole routine
     startTime = time.time()
 
-    # Load the (REGIONAL) avalanche directory from general configuration file
+    # Load the avalanche directory from general configuration file
     cfgMain = cfgUtils.getGeneralConfig()
     if avalancheDir != '':
         cfgMain['MAIN']['avalancheDir'] = avalancheDir
     else:
         avalancheDir = cfgMain['MAIN']['avalancheDir']
 
-    # Paths to the input files
-    input_rel_dir = pathlib.Path(avalancheDir) / 'Inputs' / 'REL'
-    input_shapefile = next(input_rel_dir.glob("*.shp"), None)
-    if not input_shapefile:
-        log.error(f"No shapefile found in {input_rel_dir}. Exiting.")
+    # Load input parameters from splitInputs configuration file
+    cfg = cfgUtils.getModuleConfig(sI)
+
+    # Paths to input files
+    inputRELDir = pathlib.Path(avalancheDir) / 'Inputs' / 'REL'
+    inputShp = next(inputRELDir.glob("*.shp"), None)
+    if not inputShp:
+        log.error(f"No shapefile found in {inputRELDir}.")
         return
-    input_dem = next((pathlib.Path(avalancheDir) / 'Inputs').glob("*.asc"), None)
-    if not input_dem:
-        log.error(f"No DEM file found in {pathlib.Path(avalancheDir) / 'Inputs'}. Skipping DEM processing.")
+    inputDEM = next((pathlib.Path(avalancheDir) / 'Inputs').glob("*.asc"), None)
+    if not inputDEM:
+        log.error(f"No DEM file found in {pathlib.Path(avalancheDir) / 'Inputs'}.")
         return
 
     # Create the split inputs dir
-    output_dir = pathlib.Path(avalancheDir) / 'SplitInputs'
-    output_dir.mkdir(parents=True, exist_ok=True)
+    outputDir = pathlib.Path(avalancheDir) / 'Regional'
+    outputDir.mkdir(parents=True, exist_ok=True)
 
     # Step 1: Create the central list
-    folder_list = si.createFolderList(input_shapefile)
+    folderList = sI.createFolderList(inputShp)
 
     # Step 2: Set up ava directories
     log.info("Running folder initialization for each entry...")
-    si.createFoldersForReleaseAreas(folder_list, output_dir)
+    sI.createFoldersForReleaseAreas(folderList, outputDir)
     log.info("Finished folder initialization")
 
     # Step 3: Split and move release areas to each directory
     log.info("Splitting and moving release areas...")
-    si.splitAndMoveReleaseAreas(folder_list, input_shapefile, output_dir)
+    sI.splitAndMoveReleaseAreas(folderList, inputShp, outputDir)
     log.info("Finished splitting and moving release areas")
 
     # Step 4: Clip and move DEM
-    log.info("Clipping and moving DEM...")
-    si.splitDEMByCenterpointAndMove(folder_list, input_dem, output_dir)
-    log.info("Finished clipping and moving of DEM")
+    if cfg['GENERAL'].getboolean('splitDEM'):
+        log.info("Clipping and moving DEM...")
+        sI.splitDEMByCenterpointAndMove(folderList, inputDEM, outputDir, cfg)
+        log.info("Finished clipping and moving of DEM")
 
     # Print time needed
     endTime = time.time()
-    log.info(f"Completed splitting input data into '{len(folder_list)}' individual folders after "
+    log.info(f"Completed splitting input data into '{len(folderList)}' individual folders after "
              f"{endTime - startTime:.1f} seconds.")
 
 if __name__ == '__main__':
