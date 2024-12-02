d09bc05b50dbd2d1b411906c984bd190c55c4634
paulasp <paula.spannring@gmail.com>
2024-12-02 15:25:34 +0100

    change time when output raster are written; fix bug in cellCount when calculating with generations

--- avaframe/com4FlowPy/flowCore.py
+++ avaframe/com4FlowPy/flowCore.py
@@ -438,6 +438,7 @@ def calculation(args):
             cellList = [startcell]  # list of parents for current iteration
             genList = [cellList]    # list of all cells (which are calculated), oreganised in generations
             childList = []          # list of childs of the current iteration
+            countArrayPath = np.zeros_like(dem, dtype=np.int32)
 
             for gen, cellList in enumerate(genList):
                 for idx, cell in enumerate(cellList):
@@ -473,14 +474,6 @@ def calculation(args):
                         if (nodata in dem_ng) or np.size(dem_ng) < 9:
                             continue
 
-                        # if the current child cell is already in processedCells
-                        # just add +1 to the visit-counter, else add it to the
-                        # processedCells dictionary with visit-count = 1
-                        if (row[k], col[k]) in processedCells:
-                            processedCells[(row[k], col[k])] += 1
-                        else:
-                            processedCells[(row[k], col[k])] = 1
-
                         childList.append(Cell(
                                     row[k], col[k],
                                     dem_ng, cellsize,
@@ -491,13 +484,6 @@ def calculation(args):
                                     FSI=forestArray[row[k], col[k]] if isinstance(forestArray, np.ndarray) else None,
                                     forestParams=forestParams,
                                             ))
-                if len(childList) > 0:
-                    cellList = childList
-                    genList.append(cellList)
-                    childList = []
-
-            for gen, cellList in enumerate(genList):
-                for cell in cellList:
 
                     zDeltaArray[cell.rowindex, cell.colindex] = max(zDeltaArray[cell.rowindex, cell.colindex], cell.z_delta)
                     fluxArray[cell.rowindex, cell.colindex] = max(fluxArray[cell.rowindex, cell.colindex], cell.flux)
@@ -508,8 +494,7 @@ def calculation(args):
                                                                         cell.sl_gamma)
                     travelLengthArray[cell.rowindex, cell.colindex] = max(travelLengthArray[cell.rowindex, cell.colindex],
                                                                         cell.min_distance)
-                    if processedCells[(cell.rowindex, cell.colindex)] == 1:
-                        countArray[cell.rowindex, cell.colindex] += int(1)
+                    countArrayPath[cell.rowindex, cell.colindex] = 1
 
                     # Backcalculation
                     if infraBool:
@@ -530,6 +515,12 @@ def calculation(args):
                         else:
                             forestIntArray[cell.rowindex, cell.colindex] = max(forestIntArray[cell.rowindex, cell.colindex],
                                                                             cell.forestIntCount)
+                            
+                if len(childList) > 0:
+                    cellList = childList
+                    genList.append(cellList)
+                    childList = []
+            countArray = np.add(countArray, countArrayPath)
         else:
             cellList = []
             cellList.append(startcell)
