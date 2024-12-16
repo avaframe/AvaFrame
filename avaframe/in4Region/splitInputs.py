f7795b5ec9e2d01c61bf5cb5897edff9d516f86c
leon-wagner <leonwag2002@gmail.com>
2024-12-16 11:46:02 +0100

    splitInputs "grouping" feature first step
    - Features in the input shapefile with identical names before the first underscore are added to the same folder and release shapefile

--- avaframe/in4Region/splitInputs.py
+++ avaframe/in4Region/splitInputs.py
@@ -1,8 +1,8 @@
 """Helper functions for splitting avalanche input data into individual folders"""
-
+import os
 import logging
 import shapefile  # pyshp
-from shapely.geometry import shape, Point, box
+from shapely.geometry import shape, Point, box, MultiPolygon
 from avaframe.in2Trans import ascUtils
 
 from avaframe.in3Utils.initializeProject import initializeFolderStruct
@@ -11,7 +11,7 @@ from avaframe.in3Utils.initializeProject import initializeFolderStruct
 log = logging.getLogger(__name__)
 
 def createFolderList(inputShp):
-    """Create a list of folders to be created based on the input shapefile."""
+    """Create a list of entries of each feature in the input shapefile."""
     folderList = []
     unnamedCount = 1
 
@@ -32,45 +32,70 @@ def createFolderList(inputShp):
                 'geometry': geometry,
             })
 
-    log.info(f"Created folder list with '{len(folderList)}' entries.")
+    #log.info(f"Created folder list with '{len(folderList)}' entries.")
     return folderList
 
+def groupFoldersByName(folderList):
+    """Group entries in the folderList by name. Return updated folderList"""
+    folderListGrouped = []
+
+    for entry in folderList:
+        name = entry['folderName'].split('_')[0] # Get release area name before first underscore
+        if not any(e['folderName'] == name for e in folderListGrouped):
+            folderListGrouped.append({
+                'folderName': name,
+                'features': [entry]
+            })
+        else:
+            for e in folderListGrouped:
+                if e['folderName'] == name:
+                    e['features'].append(entry)
+
+    log.info(f"Grouped '{len(folderList)}' avalanche directories with identical names")
+    log.info(f"Updated folder list contains '{len(folderListGrouped)}' entries")
+    return folderListGrouped
+
 def createFoldersForReleaseAreas(folderList, outputDir):
-    """Create folders for each release area."""
+    """Initialize avalanche folder structure in each directory within folderList"""
     for entry in folderList:
         folderName = entry['folderName']
         avaDirPath = outputDir / folderName
-
         initializeFolderStruct(str(avaDirPath), removeExisting=True)
-        log.debug(f"Created folder structure for '{folderName}'.")
-
+        #log.debug(f"Created folder structure for '{folderName}'.")
 
 def splitAndMoveReleaseAreas(folderList, inputShp, outputDir):
     """Split release areas into individual shapefiles and move them to their respective folders."""
+    # read the input shapefile
     with shapefile.Reader(inputShp) as src:
         fields = src.fields[1:]  # Skip deletion flag
         fieldNames = [field[0] for field in fields]
 
+        featuresByName = {}
         for entry in folderList:
-            folderName = entry['folderName']
-            geometry = entry['geometry']
-            avaDirPath = outputDir / folderName
-            relDir = avaDirPath / 'Inputs' / 'REL'
-            relDir.mkdir(parents=True, exist_ok=True)
-            featureOutPath = relDir / f"{folderName}.shp"
+            name = entry['folderName'].split('_')[0] # Get release area name before first underscore
+            # Group entries with the same name
+            if name not in featuresByName:
+                featuresByName[name] = []
+            featuresByName[name].extend(entry['features'])
 
-            # Write shapefile for the feature
+        # Write shapefiles to their respective folders
+        for name, features in featuresByName.items():
+            featureOutPath = outputDir / name / "Inputs" / "REL" / name
             with shapefile.Writer(str(featureOutPath)) as dst:
-                dst.fields = fields  # Copy fields
-                dst.record(*entry['properties'].values())  # Copy data
-                dst.shape(geometry)  # Copy geometry
+                dst.path = featureOutPath
+                log.debug(f"Writing shapefile to {dst.path}")
+                for field in fields: #write fields
+                    dst.field(*field)
+                for feature in features: #write geometry
+                    dst.shape(feature['geometry'])
+                    record = [feature['properties'].get(field_name, '') for field_name in fieldNames]
+                    dst.record(*record)
 
-            log.debug(f"Moved release area '{folderName}' to '{featureOutPath}'.")
-
+            log.debug(f"Saved release area to '{featureOutPath}'.")
 
 def splitDEMByCenterpointAndMove(folderList, inputDEM, outputDir, cfg):
-    """Clip the DEM around each release area's centerpoint and move it to respective folders."""
-    # Read DEM data
+    """Clip the DEM around each release scenario's centerpoint and move it to respective folders."""
+    # Read input DEM
     demData = ascUtils.readRaster(inputDEM)
     header = demData['header']
     raster = demData['rasterData']
@@ -79,21 +104,23 @@ def splitDEMByCenterpointAndMove(folderList, inputDEM, outputDir, cfg):
     xOrigin = header["xllcenter"]
     yOrigin = header["yllcenter"]
 
-    log.debug(f"Input DEM Header: {header}")
+    #log.debug(f"Input DEM Header: {header}")
 
+    # Find centerpoint
     for entry in folderList:
         folderName = entry['folderName']
-        geometry = entry['geometry']
-        center = geometry.centroid
+        for feature in entry['features']:
+            geometry = feature['geometry']
+            center = geometry.centroid
 
         # Calculate bounding box indices for clipping
-        bufferSize = float(cfg['GENERAL']['bufferSize'])
-        xmin, ymin, xmax, ymax = (
-            center.x - bufferSize,
-            center.y - bufferSize,
-            center.x + bufferSize,
-            center.y + bufferSize,
-        )
+            bufferSize = float(cfg['GENERAL']['bufferSize'])
+            xmin, ymin, xmax, ymax = (
+                center.x - bufferSize,
+                center.y - bufferSize,
+                center.x + bufferSize,
+                center.y + bufferSize,
+            )
 
         # Convert bounding box to grid indices
         colStart = max(0, int((xmin - xOrigin) / cellsize))
@@ -102,7 +129,8 @@ def splitDEMByCenterpointAndMove(folderList, inputDEM, outputDir, cfg):
         rowStart = max(0, int((yOrigin + header["nrows"] * cellsize - ymax) / cellsize))
         rowEnd = min(header["nrows"], int((yOrigin + header["nrows"] * cellsize - ymin) / cellsize))
 
-        # Flip rows for bottom-left origin
+        # Flip rows for bottom-left origin. This is needed because ASCII files can have top-left origin?
+        # needs further testing with different DEMs.
         rowStart, rowEnd = header["nrows"] - rowEnd, header["nrows"] - rowStart
 
         # Validate grid indices (happens e.g. if the bounding box falls completely outside the DEM)
