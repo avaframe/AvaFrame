f3a2ecd7f36d684a1c651340ba9dfa907d505ec9
Felix Oesterle <felix+github@alpinepics.com>
2025-02-13 19:44:05 +0100

    feat(comMotPSAInitial): Improve config file formatting and conversion (comMotPSAInitial)
      - First working conversion
    
    feat(com8MoTPSA): Enhance logging and configuration handling (com8MoTPSA) ; working commit
    
    feat(com1DFA, com8MoTPSA): Refactor simulation file handling and configuration management (comMotPSAInitialA) ; working commit

--- avaframe/com8MoTPSA/com8MoTPSA.py
+++ avaframe/com8MoTPSA/com8MoTPSA.py
@@ -1,8 +1,18 @@
 import os
 import subprocess
 import platform
+import logging
+import pandas as pd
+
+import avaframe.com1DFA.com1DFATools as com1DFATools
+import avaframe.com1DFA.com1DFA as com1DFA
+from avaframe.in3Utils import cfgUtils
+
+# create local logger
+log = logging.getLogger(__name__)
 
 
+# TODO move to whereever?
 def _runAndCheck(command):
     if os.name == "nt":
         useShell = True
@@ -33,26 +43,72 @@ def _runAndCheck(command):
             print(line)
 
 
-# Example usage
-
+# TODO move to utils
 def cfgToRcf(cfg, fileName):
-    with open(fileName, 'w') as f:
+    with open(fileName, "w") as f:
         for section in cfg.sections():
-            f.write(f"# {section.replace('_', ' ')}\n")
-            f.write("#\n")
+            if section in ("FOREST_EFFECTS", "ENTRAINMENT"):
+                pass
+            elif section in ("GENERAL", "INPUT"):
+                continue
+            else:
+                f.write(f"# {section.replace('_', ' ')}\n")
+                f.write("#\n")
             for key, value in cfg.items(section):
-                key = key.replace('_', ' ')
+                # key = key.replace('_', ' ')
                 key = key.strip()
                 f.write(f"{key:<40}{value}\n")
             f.write("#\n")
 
 
 def com8MoTPSAMain(cfgMain, cfgInfo=None):
-    # Generate command and run via subprocess.run
+    # Get all necessary information from the configuration files
 
-    rcfFile = 'outputTemplate.rcf'
+    # Load avalanche directory from general configuration file
+    avalancheDir = cfgMain["MAIN"]["avalancheDir"]
 
-    cfgToRcf(cfgInfo, rcfFile)
+    # fetch type of cfgInfo
+    typeCfgInfo = com1DFATools.checkCfgInfoType(cfgInfo)
 
-    command = ['./MoT-PSA', rcfFile]
-    _runAndCheck(command)
+    # preprocessing to create configuration objects for all simulations to run
+    simDict, outDir, inputSimFiles, simDFExisting = com1DFA.com1DFAPreprocess(cfgMain, typeCfgInfo, cfgInfo)
+
+    log.info("The following simulations will be performed")
+    for key in simDict:
+        log.info("Simulation: %s" % key)
+        exportFlag = simDict[key]["cfgSim"]["EXPORTS"].getboolean("exportData")
+
+    for key in simDict:
+        # Generate command and run via subprocess.run
+        # Configuration that needs adjustment
+
+        simDF = pd.DataFrame()
+
+        # load configuration object for current sim
+        cfg = simDict[key]["cfgSim"]
+
+        # fetch simHash for current sim
+        simHash = simDict[key]["simHash"]
+
+        # append configuration to dataframe
+        simDF = cfgUtils.appendCgf2DF(simHash, key, cfg, simDF)
+        print(simDF["DEM"])
+
+        cfgInfo["Run information"]["Area of Interest"] = cfgMain["MAIN"]["avalancheDir"]
+        cfgInfo["Run information"]["UTM zone"] = "HAUMIBLAU"
+        cfgInfo["Run information"]["EPSG geodetic datum code"] = "37888"
+        cfgInfo["Run information"]["Run name"] = cfgMain["MAIN"]["avalancheDir"]
+        cfgInfo["File names"]["Grid filename"] = "./" + str(inputSimFiles["demFile"])
+
+        # select release area input data according to chosen release scenario
+        # inputSimFiles = gI.selectReleaseFile(inputSimFiles, cfg["INPUT"]["releaseScenario"])
+
+        # create required input from input files
+        # demOri, inputSimLines = prepareInputData(inputSimFiles, cfg)
+
+        rcfFile = key + ".rcf"
+
+        cfgToRcf(cfgInfo, rcfFile)
+
+    # command = ['./MoT-PSA', rcfFile]
+    # _runAndCheck(command)
