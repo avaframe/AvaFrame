c3357e6080cfeda0ec2af634aab121382023e3f3
Felix Oesterle <felix+github@alpinepics.com>
2025-02-13 16:15:35 +0100

    feat(comMotPSAInitial): Add initial com8MoTPSA module
    Initial implementation of the com8MoTPSA module.  Includes
    a basic run script, configuration file parsing, and a
    function to convert the configuration file to the RCF format
    required by the MoT-PSA binary.  A README_dev file is also
    added with basic instructions.

---
+++ avaframe/com8MoTPSA/com8MoTPSA.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+import platform
+
+
+def _runAndCheck(command):
+    if os.name == "nt":
+        useShell = True
+    elif platform.system() == "Darwin":
+        useShell = False
+    else:
+        useShell = False
+
+    # This starts the subprocess
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=useShell,
+        encoding="utf-8",
+        errors="replace",
+        universal_newlines=True,
+    )
+
+    while True:
+        realtimeOutput = process.stdout.readline()
+
+        if realtimeOutput == "" and process.poll() is not None:
+            break
+
+        if realtimeOutput:
+            line = realtimeOutput.strip()
+            print(line)
+
+
+# Example usage
+
+def cfgToRcf(cfg, fileName):
+    with open(fileName, 'w') as f:
+        for section in cfg.sections():
+            f.write(f"# {section.replace('_', ' ')}\n")
+            f.write("#\n")
+            for key, value in cfg.items(section):
+                key = key.replace('_', ' ')
+                key = key.strip()
+                f.write(f"{key:<40}{value}\n")
+            f.write("#\n")
+
+
+def com8MoTPSAMain(cfgMain, cfgInfo=None):
+    # Generate command and run via subprocess.run
+
+    rcfFile = 'outputTemplate.rcf'
+
+    cfgToRcf(cfgInfo, rcfFile)
+
+    command = ['./MoT-PSA', rcfFile]
+    _runAndCheck(command)
