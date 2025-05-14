fb71ff83ef26534df6c6a06f9cc7be84a8f3e793
Felix Oesterle <felix+github@alpinepics.com>
2025-05-14 15:44:16 +0200

    fix(shpConversion): improve error messages for shape file validation

--- avaframe/in2Trans/shpConversion.py
+++ avaframe/in2Trans/shpConversion.py
@@ -302,11 +302,13 @@ def readLine(fname, defname, dem):
         Lx = (coordx[i] - header["xllcenter"]) / header["cellsize"]
         Ly = (coordy[i] - header["yllcenter"]) / header["cellsize"]
         if (Ly < 0) or (Ly > header["nrows"] - 1) or (Lx < 0) or (Lx > header["ncols"] - 1):
-            log.error(fname)
-            raise ValueError("This shape file exceeds dem extent. This is not allowed")
+            message = "This shape file exceeds dem extent: %s " % fname
+            log.error(message)
+            raise ValueError(message)
         elif np.isnan(rasterDEM[int(np.floor(Ly)), int(np.floor(Lx))]):
-            log.error(fname)
-            raise ValueError("This shape file is at least partially outside of DEM, this is not allowed!")
+            message = "This shape file is at least partially outside of dem extent: %s " % fname
+            log.error(message)
+            raise ValueError(message)
     return Line
 
 
