d91c48daa507bebc803d03fd3ffa396b70115b9e
PaulaSp3 <paula.spannring@gmail.com>
2025-08-13 17:44:27 +0200

    partially working for a hydrograph with timestep 0 and without REL

--- avaframe/com1DFA/com1DFA.py
+++ avaframe/com1DFA/com1DFA.py
@@ -471,7 +471,10 @@ def prepareReleaseEntrainment(cfg, rel, inputSimLines):
         )
 
     # set release thickness
-    if cfg["GENERAL"].getboolean("relThFromFile") is False:
+    if cfg["GENERAL"].getboolean("hydrograph") and cfg["GENERAL"].getboolean("noRelArea"):
+        inputSimLines["hydrographLine"]["thicknessSource"] = ["csv file"]
+        log.info("Release scenario with hydrograph and without REL area.")
+    elif cfg["GENERAL"].getboolean("relThFromFile") is False:
         releaseLine = setThickness(cfg, inputSimLines["releaseLine"], "relTh")
         inputSimLines["releaseLine"] = releaseLine
     log.debug("Release area scenario: %s - perform simulations" % (relName))
@@ -595,6 +598,11 @@ def prepareInputData(inputSimFiles, cfg):
     releaseLine = shpConv.readLine(relFile, "release1", demOri)
     releaseLine["file"] = relFile
     releaseLine["type"] = "Release"
+    if cfg["GENERAL"].getboolean("hydrograph") and cfg["GENERAL"].getboolean("noRelArea"):
+        releaseLine["type"] = "Hydrograph"
+        hydrValues = gI.getHydrographCsv(inputSimFiles["hydrographCsv"])
+        releaseLine["thickness"] = hydrValues["thickness"][hydrValues["timeStep"] == 0]
+        releaseLine["thicknessSource"] = ["csv file"]
     # check for holes in release area polygons
     gI.checkForMultiplePartsShpArea(cfg["GENERAL"]["avalancheDir"], releaseLine, "com1DFA", type="release")
 
@@ -668,7 +676,6 @@ def prepareInputData(inputSimFiles, cfg):
     if cfg["GENERAL"].getboolean("hydrograph"):
         try:
             hydrFile = inputSimFiles["hydrographFile"]
-            print(hydrFile)
             hydrLine = shpConv.readLine(hydrFile, "", demOri)
             hydrLine["fileName"] = hydrFile
             hydrLine["type"] = "Hydrograph"
@@ -1406,7 +1413,6 @@ def initializeParticles(cfg, releaseLine, dem, inputSimLines="", logName="", rel
     # initialize time
     t = 0
     particles["t"] = t
-
     relCells = np.size(indRelY)
     partPerCell = particles["nPart"] / relCells
 
